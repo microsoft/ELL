34a078103c66978c0274aec19ecb92b8e1fa6290
Ofer Dekel <oferd@microsoft.com>
2015-11-28 08:16:03 -0800

    Added untracked file

---
+++ libraries/mappings/src/Coordinate.cpp
@@ -0,0 +1,31 @@
+// Coordinate.cpp
+
+#include "Coordinate.h"
+
+namespace mappings
+{
+    Coordinate::Coordinate(uint64 row, uint64 column) : _row(row), _column(column)
+    {}
+
+    uint64 Coordinate::GetRow() const
+    {
+        return _row;
+    }
+
+    uint64 Coordinate::GetColumn() const
+    {
+        return _column;
+    }
+
+    void Coordinate::Serialize(JsonSerializer& serializer) const
+    {
+        serializer.Write("r", _row); 
+        serializer.Write("c", _column);
+    }
+
+    void Coordinate::Deserialize(JsonSerializer& serializer)
+    {
+        serializer.Read("r", _row);
+        serializer.Read("c", _column);
+    }
+}
