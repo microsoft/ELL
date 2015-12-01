a42c69d1d07459e5a6279e35f71b63a48e3421b8
Ofer Dekel <oferd@microsoft.com>
2015-12-01 07:10:11 -0800

    edits to SVG

--- print/src/main.cpp
+++ print/src/main.cpp
@@ -47,90 +47,96 @@ int main(int argc, char* argv[])
 <body>
 
 <style>
-	text
-	{
-		font-family:	sans-serif;
-	}
+    text
+    {
+        font-family:    sans-serif;
+    }
 
-            	rect
-	{
-		stroke:		#333333;
-		stroke-width:	2;
-	}
-	
-	rect.Interior
-	{
-		fill:		white;
-	}
+                            rect
+    {
+        stroke:        #222222;
+        stroke-width:    2;
+    }
+    
+    rect.Interior
+    {
+        fill:        white;
+    }
 
-            	rect.ZeroLayer
-	{
-		fill:		#22AA22;
-	}
+                            rect.ZeroLayer
+    {
+        fill:        #22AA22;
+    }
 
-            	rect.ScaleLayer
-	{
-		fill:		#BB3333;
-	}
+                            rect.ScaleLayer
+    {
+        fill:        #FF2244;
+    }
 
-            	ellipse.Connector
-	{
-		stroke:		#333333;
-		stroke-width:	3;	
-	}
+                            ellipse.Connector
+    {
+        fill:        #222222;
+    }
 
-            </style>
+                path
+    {
+        stroke:        #110011;
+        stroke-width:    2;    
+        fill:        none;
+        stroke-dasharray:    4,2;
+    }
 
-            <svg width="800" height="300">
+                        </style>
 
-            	<defs>
-		<g id="Node">
-			<ellipse class "Connector" cx="25" cy="0" rx="5" ry="5" />
-			<ellipse class "Connector" cx="25" cy="40" rx="5" ry="5" />
-			<rect class="Interior" x="0" y="0" rx="10" ry="10" width="50" height="40"/>
-		</g>
-	</defs>
+                        <svg width="800" height="300">
 
-            	<rect class="ZeroLayer" x="20" y="20" rx="10" ry="10" width="760" height="60"/>
-	<text x="35" y="55" text-anchor="middle" fill="white" font-size="15" font-weight="bold">1</text>
-	<text x="65" y="50" text-anchor="middle" fill="white" font-size="15" font-weight="bold" transform="rotate(-90,65,50)">INPUT</text>
+                            <defs>
+        <g id="Node">
+            <ellipse class="Connector" cx="25" cy="0" rx="5" ry="5" />
+            <ellipse class="Connector" cx="25" cy="40" rx="5" ry="5" />
+            <rect class="Interior" x="0" y="0" rx="10" ry="10" width="50" height="40"/>
+        </g>
+    </defs>
 
-            <use xlink : href = "#Node" x = "80" y = "30" rx = "10" ry = "10"/>
+                            <rect class="ZeroLayer" x="20" y="20" rx="10" ry="10" width="760" height="60"/>
+        <text x="35" y="55" text-anchor="middle" fill="white" font-size="15" font-weight="bold">1</text>
+        <text x="65" y="50" text-anchor="middle" fill="white" font-size="15" font-weight="bold" transform="rotate(-90,65,50)">INPUT</text>
+
+                        <use xlink : href = "#Node" x = "80" y = "30" rx = "10" ry = "10"/>
             <text x = "105" y = "55" text-anchor = "middle">457.8</text>
 
-            <use xlink : href = "#Node" x = "135" y = "30" rx = "10" ry = "10"/>
+                        <use xlink : href = "#Node" x = "135" y = "30" rx = "10" ry = "10"/>
             <text x = "160" y = "55" text-anchor = "middle">-12</text>
 
-            <use xlink : href = "#Node" x = "190" y = "30" rx = "10" ry = "10"/>
+                        <use xlink : href = "#Node" x = "190" y = "30" rx = "10" ry = "10"/>
 
-            <use xlink : href = "#Node" x = "245" y = "30" rx = "10" ry = "10"/>
+                        <use xlink : href = "#Node" x = "245" y = "30" rx = "10" ry = "10"/>
 
-            <use xlink : href = "#Node" x = "300" y = "30" rx = "10" ry = "10"/>
+                        <use xlink : href = "#Node" x = "300" y = "30" rx = "10" ry = "10"/>
 
-            <rect class = "ScaleLayer" x = "20" y = "100" rx = "10" ry = "10" width = "760" height = "60"/>
+                        <rect class = "ScaleLayer" x = "20" y = "100" rx = "10" ry = "10" width = "760" height = "60"/>
             <text x = "35" y = "135" text-anchor = "middle" fill = "white" font-size = "15" font-weight = "bold">2</text>
             <text x = "65" y = "130" text-anchor = "middle" fill = "white" font-size = "15" font-weight = "bold" transform = "rotate(-90,65,130)">SUM</text>
 
-            <use xlink : href = "#Node" x = "80" y = "110" rx = "10" ry = "10"/>
+                        <use xlink : href = "#Node" x = "80" y = "110" rx = "10" ry = "10"/>
             <text x = "105" y = "55" text-anchor = "middle">457.8</text>
 
-            <use xlink : href = "#Node" x = "135" y = "110" rx = "10" ry = "10"/>
+                        <use xlink : href = "#Node" x = "135" y = "110" rx = "10" ry = "10"/>
             <text x = "160" y = "55" text-anchor = "middle">-12</text>
 
-            <use xlink : href = "#Node" x = "190" y = "110" rx = "10" ry = "10"/>
+                        <use xlink : href = "#Node" x = "190" y = "110" rx = "10" ry = "10"/>
 
-            <use xlink : href = "#Node" x = "245" y = "110" rx = "10" ry = "10"/>
+                        <use xlink : href = "#Node" x = "245" y = "110" rx = "10" ry = "10"/>
 
-            <use xlink : href = "#Node" x = "300" y = "110" rx = "10" ry = "10"/>
+                        <use xlink : href = "#Node" x = "300" y = "110" rx = "10" ry = "10"/>
 
+            <path d="M 105 75 q 10 15 20 15 h 15 q 10 0 20 15"/>
+            <path d="M 105 75 q 10 15 20 15 h 70 q 10 0 20 15"/>
 
-
-            </svg>
-
-
-            </html>
-            </body>
-            )aw";
+        </svg>
+</html>
+</body>
+)aw";
 
     }
     catch (runtime_error e)
