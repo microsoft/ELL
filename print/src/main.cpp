d29cf7ade258df0ecc01fdd1ab06de4950aa87de
Ofer Dekel <oferd@microsoft.com>
2015-11-30 17:51:55 -0800

    added some svg code

--- print/src/main.cpp
+++ print/src/main.cpp
@@ -40,7 +40,98 @@ int main(int argc, char* argv[])
         auto layersFile = argv[1];
         ifstream layersFStream = OpenIfstream(layersFile);
         auto map = Map::Deserialize<PrintableMap>(layersFStream);
-        map->Print(cout);
+        // map->Print(cout);
+
+        cout << R"aw(
+<html>
+<body>
+
+<style>
+	text
+	{
+		font-family:	sans-serif;
+	}
+
+            	rect
+	{
+		stroke:		#333333;
+		stroke-width:	2;
+	}
+	
+	rect.Interior
+	{
+		fill:		white;
+	}
+
+            	rect.ZeroLayer
+	{
+		fill:		#22AA22;
+	}
+
+            	rect.ScaleLayer
+	{
+		fill:		#BB3333;
+	}
+
+            	ellipse.Connector
+	{
+		stroke:		#333333;
+		stroke-width:	3;	
+	}
+
+            </style>
+
+            <svg width="800" height="300">
+
+            	<defs>
+		<g id="Node">
+			<ellipse class "Connector" cx="25" cy="0" rx="5" ry="5" />
+			<ellipse class "Connector" cx="25" cy="40" rx="5" ry="5" />
+			<rect class="Interior" x="0" y="0" rx="10" ry="10" width="50" height="40"/>
+		</g>
+	</defs>
+
+            	<rect class="ZeroLayer" x="20" y="20" rx="10" ry="10" width="760" height="60"/>
+	<text x="35" y="55" text-anchor="middle" fill="white" font-size="15" font-weight="bold">1</text>
+	<text x="65" y="50" text-anchor="middle" fill="white" font-size="15" font-weight="bold" transform="rotate(-90,65,50)">INPUT</text>
+
+            <use xlink : href = "#Node" x = "80" y = "30" rx = "10" ry = "10"/>
+            <text x = "105" y = "55" text-anchor = "middle">457.8</text>
+
+            <use xlink : href = "#Node" x = "135" y = "30" rx = "10" ry = "10"/>
+            <text x = "160" y = "55" text-anchor = "middle">-12</text>
+
+            <use xlink : href = "#Node" x = "190" y = "30" rx = "10" ry = "10"/>
+
+            <use xlink : href = "#Node" x = "245" y = "30" rx = "10" ry = "10"/>
+
+            <use xlink : href = "#Node" x = "300" y = "30" rx = "10" ry = "10"/>
+
+            <rect class = "ScaleLayer" x = "20" y = "100" rx = "10" ry = "10" width = "760" height = "60"/>
+            <text x = "35" y = "135" text-anchor = "middle" fill = "white" font-size = "15" font-weight = "bold">2</text>
+            <text x = "65" y = "130" text-anchor = "middle" fill = "white" font-size = "15" font-weight = "bold" transform = "rotate(-90,65,130)">SUM</text>
+
+            <use xlink : href = "#Node" x = "80" y = "110" rx = "10" ry = "10"/>
+            <text x = "105" y = "55" text-anchor = "middle">457.8</text>
+
+            <use xlink : href = "#Node" x = "135" y = "110" rx = "10" ry = "10"/>
+            <text x = "160" y = "55" text-anchor = "middle">-12</text>
+
+            <use xlink : href = "#Node" x = "190" y = "110" rx = "10" ry = "10"/>
+
+            <use xlink : href = "#Node" x = "245" y = "110" rx = "10" ry = "10"/>
+
+            <use xlink : href = "#Node" x = "300" y = "110" rx = "10" ry = "10"/>
+
+
+
+            </svg>
+
+
+            </html>
+            </body>
+            )aw";
+
     }
     catch (runtime_error e)
     {
