45b4d2a457a16bd62a3be789c64d33d9edf99163
Ofer Dekel <oferd@microsoft.com>
2015-12-31 02:43:19 -0800

    adding missing files

---
+++ scripts/test.py
@@ -0,0 +1,12 @@
+import os
+
+workingDirectory = "c:/tmp"
+libraryRoot = "C:/Users/OferD/Source/Repos/Bonsai"
+
+trainerExecutablePath = os.path.abspath(os.path.join(libraryRoot, "trainers/sgd/bin/debug/sgd.exe"))
+trainingDataPath = os.path.abspath(os.path.join(libraryRoot, "data/a9a.txt"))
+outputMapFile1 = os.path.abspath(os.path.join(workingDirectory, "output1.map"))
+outputMapFile2 = os.path.abspath(os.path.join(workingDirectory, "output2.map"))
+
+os.system("%s -df %s -omf %s" %(trainerExecutablePath, trainingDataPath, outputMapFile1))
+os.system("%s -df %s -imf %s -omf %s" %(trainerExecutablePath, trainingDataPath, outputMapFile1, outputMapFile2))
