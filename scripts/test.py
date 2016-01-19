cbd8b7fda77f5b2e2bd5d895cbe0f5f125c5e7f4
Ofer Dekel <oferd@microsoft.com>
2016-01-18 21:36:56 -0800

    moved binaries to global directory build/bin allowed GetDataIterator to work even if map file not provided

--- scripts/test.py
+++ scripts/test.py
@@ -1,15 +1,13 @@
 import os
 
-workingDirectory = "c:/tmp"
-libraryRoot = "C:/Users/OferD/Source/Repos/Bonsai"
+binDir = "C:/Users/OferD/Source/Repos/Bonsai/build/bin/Debug"
+dataDir = "C:/Users/OferD/Source/Repos/Bonsai/data"
 
-trainerExecutablePath = os.path.abspath(os.path.join(libraryRoot, "trainers/sgd/bin/debug/sgd.exe"))
-printExecutablePath = os.path.abspath(os.path.join(libraryRoot, "print/bin/debug/print.exe"))
-trainingDataPath = os.path.abspath(os.path.join(libraryRoot, "data/a9a.txt"))
-outputMapFile1 = os.path.abspath(os.path.join(workingDirectory, "output1.map"))
-outputMapFile2 = os.path.abspath(os.path.join(workingDirectory, "output2.map"))
-outputSvgFile = os.path.abspath(os.path.join(workingDirectory, "print.html"))
+trainingDataPath = os.path.abspath(os.path.join(dataDir, "a9a.txt"))
+outputMapFile1 = os.path.abspath(os.path.join(binDir, "output1.map"))
+outputMapFile2 = os.path.abspath(os.path.join(binDir, "output2.map"))
+outputSvgFile = os.path.abspath(os.path.join(binDir, "print.html"))
 
-os.system("%s -idf %s -omf %s" %(trainerExecutablePath, trainingDataPath, outputMapFile1))
-os.system("%s -idf %s -imf %s -omf %s -imis 2" %(trainerExecutablePath, trainingDataPath, outputMapFile1, outputMapFile2))
-os.system("%s -imf %s -osf %s" %(printExecutablePath, outputMapFile2, outputSvgFile))
+os.system("%s -idf %s -omf %s" %(os.path.join(binDir, "sgd.exe"), trainingDataPath, outputMapFile1))
+os.system("%s -idf %s -imf %s -omf %s -imis 2" %(os.path.join(binDir, "sgd.exe"), trainingDataPath, outputMapFile1, outputMapFile2))
+os.system("%s -imf %s -osf %s" %(os.path.join(binDir, "print.exe"), outputMapFile2, outputSvgFile))
