d50c8113db3471c59cb7d41641ca5d325a013752
Charles Jacobs <cjacobs@microsoft.com>
2016-02-01 16:20:16 -0800

    Fixed commandline argument in train_and_print script

--- scripts/train_and_print.py
+++ scripts/train_and_print.py
@@ -6,7 +6,7 @@ mapFile2 = args[2]
 outFile  = args[3]
 
 runBinary('sgd', ['-idf', dataFile, '-omf', mapFile1])
-runBinary('sgd', ['-idf', dataFile, '-imf', mapFile1, '-omf', mapFile2, '-imis', '2'])
+runBinary('sgd', ['-idf', dataFile, '-imf', mapFile1, '-omf', mapFile2, '-clis', '2'])
 
 if outFile:
     runBinary('print', ['-imf', mapFile2, '-osf', outFile])
