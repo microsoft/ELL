e84a51ef9b331ab18778bce072bacbe57cd1664e
Ofer Dekel <oferd@microsoft.com>
2016-02-25 00:21:16 -0800

    reformat documentation in loss functions lib

--- libraries/lossFunctions/include/HingeLoss.h
+++ libraries/lossFunctions/include/HingeLoss.h
@@ -12,18 +12,26 @@
 
 namespace lossFunctions
 {
-    /// Implements the hinge loss function: loss(x) = max(1-x, 0)
-    ///
+    /// <summary> Implements the hinge loss function: loss(x) = max(1-x, 0) </summary>
     class HingeLoss
     {
 
     public:
-        /// \returns The value of the loss at a given point
+
+        /// <summary> Returns the value of the loss at a given point. </summary>
         ///
+        /// <param name="prediction"> The prediction. </param>
+        /// <param name="label"> The label. </param>
+        ///
+        /// <returns> The loss. </returns>
         double Evaluate(double prediction, double label) const;
 
-        /// \returns the value of the loss derivative at a given point
+        /// <summary> Returns the value of the loss derivative at a given point. </summary>
         ///
+        /// <param name="prediction"> The prediction. </param>
+        /// <param name="label"> The label. </param>
+        ///
+        /// <returns> The loss derivative. </returns>
         double GetDerivative(double prediction, double label) const;
     };
 }
