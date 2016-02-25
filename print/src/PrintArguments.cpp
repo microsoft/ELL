98be80ca7fecc01f42bff60fc1b74a614cdbe309
Ofer Dekel <oferd@microsoft.com>
2016-02-25 11:23:35 -0800

    added missing file

---
+++ print/src/PrintArguments.cpp
@@ -0,0 +1,72 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Project:  [projectName]
+//  File:     PrintArguments.cpp (print)
+//  Authors:  Ofer Dekel
+//
+//  [copyright]
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include "PrintArguments.h"
+
+// utilities
+#include "files.h"
+
+void ParsedPrintArguments::AddArgs(utilities::CommandLineParser & parser)
+{
+    parser.AddOption(inputMapFile, "inputMapFile", "imf", "Path to the input file that contains the map information", "");
+    parser.AddOption(outputSvgFile, "outputSvgFile", "osf", "Path to the output Svg file", "");
+
+    parser.AddOption(valueElementLayout.width, "valueElementWidth", "vew", "Width of each element in a layer that shows values", 55);
+    parser.AddOption(valueElementLayout.height, "valueElementHeight", "veh", "Height of each element in a layer that shows values", 40);
+    parser.AddOption(valueElementLayout.leftPadding, "valueElementLeftPadding", "velp", "Horizontal distance between the left edge of a layer that shows values and its first element", 70);
+    parser.AddOption(valueElementLayout.rightPadding, "valueElementRightPadding", "verp", "Horizontal distance between the right edge of a layer that shows values and its last element", 10);
+    parser.AddOption(valueElementLayout.verticalPadding, "valueElementVerticalPadding", "vevp", "Vertical distance between the top/bottom edges of a layer that shows values and the top/bottom edges of its elements", 14);
+    parser.AddOption(valueElementLayout.horizontalSpacing, "valueElementHorizontalSpacing", "vehs", "Horizontal distance between consecutive elements in a layer that shows values", 5);
+    parser.AddOption(valueElementLayout.dotsWidth, "valueElementDotsWidth", "vedw", "Minimum width of the gap used to show the three dots, which show up when some of the elements are hidden", 45);
+
+    parser.AddOption(valueElementStyle.cornerRadius, "valueElementCornerRadius", "vecr", "Radius of rounded corners of elements in a layer that shows values", 5);
+    parser.AddOption(valueElementStyle.connectorRadius, "valueElementConnectorRadius", "vekr", "Radius of connectors on top and bottom of elements in a layer that shows values", 5);
+    parser.AddOption(valueElementStyle.maxChars, "valueElementMaxChars", "vemc", "Number of characters used to print the value in each element", 6);
+
+    parser.AddOption(emptyElementLayout.width, "emptyElementWidth", "eew", "Width of each element in a layer that doesn't show values", 40);
+    parser.AddOption(emptyElementLayout.height, "emptyElementHeight", "eeh", "Height of each element in a layer that doesn't show values", 28);
+    parser.AddOption(emptyElementLayout.leftPadding, "emptyElementLeftPadding", "eelp", "Horizontal distance between the left edge of a layer that doesn't show values and its first element", 70);
+    parser.AddOption(emptyElementLayout.rightPadding, "emptyElementRightPadding", "eerp", "Horizontal distance between the right edge of a layer that doesn't show values and its last element", 10);
+    parser.AddOption(emptyElementLayout.verticalPadding, "emptyElementVerticalPadding", "eevp", "Vertical distance between the top/bottom edges of a layer that doesn't show values and the top/bottom edges of its elements", 18);
+    parser.AddOption(emptyElementLayout.horizontalSpacing, "emptyElementHorizontalSpacing", "eehs", "Horizontal distance between consecutive elements in a layer that doesn't show values", 5);
+    parser.AddOption(emptyElementLayout.dotsWidth, "emptyElementDotsWidth", "eedw", "Minimum width of the gap used to show the three dots, which show up when some of the elements are hidden", 45);
+
+    parser.AddOption(emptyElementStyle.cornerRadius, "emptyElementCornerRadius", "eecr", "Radius of rounded corners of elements in a layer that doesn't show values", 5);
+    parser.AddOption(emptyElementStyle.connectorRadius, "emptyElementConnectorRadius", "eekr", "Radius of connectors on top and bottom of elements in a layer that doesn't show values", 5);
+
+    parser.AddOption(mapLayout.horizontalMargin, "layerHorizontalMargin", "lhm", "Horizontal distance to the left edge of each layer", 20);
+    parser.AddOption(mapLayout.verticalMargin, "layerVerticalMargin", "lvm", "Vertical distnace to the top edge of the first layer", 10);
+    parser.AddOption(mapLayout.verticalSpacing, "layerVerticalSpacing", "lvs", "The amount of vertical space between layers", 30);
+
+    parser.AddOption(layerStyle.maxWidth, "layerMaxWidth", "lmw", "The maximum width of any layer", 700);
+    parser.AddOption(layerStyle.cornerRadius, "layerCornerRadius", "lcr", "Radius of layer rounded corners", 10);
+
+    parser.AddOption(edgeStyle.flattness, "edgeFlattness", "ef", "Flatness of edges: between 0 and 1", 0.65);
+    parser.AddOption(edgeStyle.dashStyle, "edgeDashStyle", "eds", "The dash style of the edges", "5,2");
+}
+
+utilities::ParseResult ParsedPrintArguments::PostProcess(const utilities::CommandLineParser & parser)
+{
+    std::vector<std::string> parseErrorMessages;
+
+    if(inputMapFile == "")
+    {
+        parseErrorMessages.push_back("-inputMapFile (or -imf) is required");
+    }
+    else
+    {
+        if(!utilities::IsFileReadable(inputMapFile))
+        {
+            parseErrorMessages.push_back("cannot read from specified input map file: " + inputMapFile);
+        }
+    }
+
+    return parseErrorMessages;
+}
