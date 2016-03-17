927ad7a48057f3b48cf45b456eab9e199f3a0004
Charles Jacobs <cjacobs@microsoft.com>
2016-03-17 13:05:19 -0700

    Removed empty map unit test

--- libraries/layers/test/test.cpp
+++ libraries/layers/test/test.cpp
@@ -8,9 +8,6 @@
 //
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// layers
-#include "Map.h"
-
 // testing
 #include "testing.h"
 
@@ -19,19 +16,10 @@
 #include <string>
 
 
-void TestMap()
-{
-    float val1 = 0;
-    float val2 = 0;
-    testing::ProcessTest("Comparing ", testing::IsEqual(val1, val2, 1.0e-8));
-}
-
 /// Runs all tests
 ///
 int main()
 {
-    TestMap();
-
     if (testing::DidTestFail())
     {
         return 1;
