23a9a92bd4adba3e5324ec2a4ce3abeba8d4cf77
Ofer Dekel <oferd@microsoft.com>
2016-03-23 18:55:12 -0700

    supervisedExample is not IDataVector

--- libraries/dataset/include/SupervisedExample.h
+++ libraries/dataset/include/SupervisedExample.h
@@ -24,12 +24,9 @@
 namespace dataset
 {
     /// <summary> A supervised example. </summary>
-    class SupervisedExample : public IDataVector
+    class SupervisedExample 
     {
     public:
-
-        using IVector::Dot;
-        using IVector::AddTo;
 
         /// <summary> Constructs a supervised example. </summary>
         ///
@@ -49,57 +46,73 @@ namespace dataset
         double GetLabel() const;
 
         /// <summary> Inherited via IDataVector. </summary>
-        ///
-        /// <returns> The type. </returns>
-        virtual type GetType() const override;
-
-        /// <summary> Inherited via IDataVector. </summary>
-        virtual void Reset() override;
+        void Reset();
 
         /// <summary> Inherited via IDataVector. </summary>
         ///
         /// <param name="index"> Zero-based index of the. </param>
         /// <param name="value"> The value. </param>
-        virtual void PushBack(uint64 index, double value = 1.0) override;
+        void PushBack(uint64 index, double value = 1.0);
 
         /// <summary> Inherited via IDataVector. </summary>
         ///
         /// <returns> An uint64. </returns>
-        virtual uint64 Size() const override;
+        uint64 Size() const;
 
         /// <summary> \returns The number of non-zeros. </summary>
         ///
         /// <returns> The total number of nonzeros. </returns>
-        virtual uint64 NumNonzeros() const override;
+        uint64 NumNonzeros() const;
 
         /// <summary> Inherited via IDataVector. </summary>
         ///
         /// <returns> A double. </returns>
-        virtual double Norm2() const override;
+        double Norm2() const;
 
-        /// <summary> Inherited via IDataVector. </summary>
+        /// <summary> Adds a weighted version of this vector into another vector. </summary>
         ///
-        /// <param name="p_other"> [in,out] If non-null, the other. </param>
-        /// <param name="scalar"> The scalar. </param>
-        virtual void AddTo(double * p_other, double scalar = 1.0) const override;
+        /// <param name="p_other"> [in,out] The other vector. </param>
+        /// <param name="scalar"> The weight. </param>
+        void AddTo(double * p_other, double scalar = 1.0) const;
 
-        /// <summary> Inherited via IDataVector. </summary>
+        /// <summary> Adds a weighted version of this vector into another vector. </summary>
         ///
-        /// <param name="p_other"> The other. </param>
+        /// <param name="other"> [in,out] The other vector. </param>
+        /// <param name="scalar"> The weight. </param>
+        void AddTo(std::vector<double>& other, double scalar = 1.0) const;
+
+        /// <summary> Computes the dot product with another vector. </summary>
         ///
-        /// <returns> A double. </returns>
-        virtual double Dot(const double * p_other) const override;
+        /// <param name="p_other"> The other vector. </param>
+        ///
+        /// <returns> The result. </returns>
+        double Dot(const double * p_other) const;
+
+        /// <summary> Computes the dot product with another vector. </summary>
+        ///
+        /// <param name="p_other"> The other vector. </param>
+        ///
+        /// <returns> The result. </returns>
+        double Dot(const std::vector<double>& other) const;
 
         /// <summary> Prints the datavector to an output stream. </summary>
         ///
         /// <param name="os"> [in,out] Stream to write data to. </param>
-        virtual void Print(std::ostream& os) const override;
+        void Print(std::ostream& os) const;
 
     private:
         std::unique_ptr<IDataVector> _upInstance;
         double _weight;
         double _label;
     };
+
+    /// <summary> Stream insertion operator. </summary>
+    ///
+    /// <param name="ostream"> [in,out] Stream to write data to. </param>
+    /// <param name="example"> The example. </param>
+    ///
+    /// <returns> The shifted ostream. </returns>
+    std::ostream& operator<<(std::ostream& ostream, const SupervisedExample& example);
 
     using RowDataset = linear::RowMatrix<SupervisedExample>;
 }
