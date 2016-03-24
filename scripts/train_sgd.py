b9f4452b68e115a7bce7479fdb0a97e077b6f535
Charles Jacobs <cjacobs@mac.com>
2016-03-23 21:47:27 -0700

    Changed train_and_print to train_sgd, which trains a single sgd layer, prints it, and compiles it. Appears to be broken at the moment.

---
+++ scripts/train_sgd.py
@@ -0,0 +1,15 @@
+﻿from recipeUtil import *
+
+dataFile = args[0]
+stackFile1 = args[1]
+htmlOutFile = args[2]
+codeOutFile = args[3]
+
+# train using sgd
+runBinary('sgd', ['-idf', dataFile, '-osf', stackFile1])
+
+# print to svg file
+runBinary('print', ['-isf', stackFile1, '-osvg', htmlOutFile])
+
+# compile to C code
+#runBinary('compile', ['-isf', stackFile1, '-ocf', codeOutFile])
