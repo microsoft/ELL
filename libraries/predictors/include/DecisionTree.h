7d174644b855c2b607ea980f01bb50e917f43c5c
Ofer Dekel <oferd@microsoft.com>
2016-04-26 15:49:12 -0700

    removed old iterators renamed members

--- libraries/predictors/include/DecisionTree.h
+++ libraries/predictors/include/DecisionTree.h
@@ -16,7 +16,7 @@
 namespace decisionTree
 {
     /// <summary>
-    /// Represents a growable binary decision tree with interval splitting rules and outputs in all
+    /// Represents a binary decision tree with threshold split rules and output values in all
     /// vertices.
     /// </summary>
     class DecisionTree
@@ -24,30 +24,30 @@ namespace decisionTree
 
     public:
         
-        /// <summary> Represents a SplitRule in a decision tree. </summary>
+        /// <summary> Represents a split rule in a decision tree. </summary>
         class SplitRule
         {
         public:
 
             /// <summary> Constructs a SplitRule. </summary>
             ///
-            /// <param name="index"> Zero-based index of the. </param>
-            /// <param name="value"> The value. </param>
-            SplitRule(int index, double value);
+            /// <param name="featureIndex"> Zero-based index of the split feature. </param>
+            /// <param name="threshold"> The threshold value. </param>
+            SplitRule(int featureIndex, double threshold);
 
-            /// <summary> Returns the index. </summary>
+            /// <summary> Returns the feature index. </summary>
             ///
-            /// <returns> The index. </returns>
-            int GetIndex() const;
+            /// <returns> The feature index. </returns>
+            int GetFeatureIndex() const;
 
-            /// <summary> Returns the value. </summary>
+            /// <summary> Returns the threshold value. </summary>
             ///
-            /// <returns> A double. </returns>
-            double Get() const;
+            /// <returns> A threshold. </returns>
+            double GetThreshold() const;
 
         private:
-            int _index;
-            double _value;
+            int _featureIndex;
+            double _threshold;
         };
 
         /// <summary> Represents a pair of Children of a node in a binary tree. </summary>
@@ -57,132 +57,36 @@ namespace decisionTree
 
             /// <summary> Constructs the pair of Children. </summary>
             ///
-            /// <param name="child0"> Child 0. </param>
-            /// <param name="child1"> Child 1. </param>
-            ChildPair(int child0, int child1);
+            /// <param name="negativeChildIndex"> Index of the child that corresponds to instances where the split rule is negative. </param>
+            /// <param name="positiveChildIndex"> Index of the child that corresponds to instances where the split rule is positive. </param>
+            ChildPair(int negativeChildIndex, int positiveChildIndex);
 
-            /// <summary> Returns Child 0. </summary>
+            /// <summary> Returns index of the negative child. </summary>
             ///
-            /// <returns> Child 0. </returns>
-            int GetChild0() const;
+            /// <returns> Index of the negative child. </returns>
+            int GetNegativeChildIndex() const;
 
-            /// <summary> Returns Child 1. </summary>
+            /// <summary> Returns index of the positive child. </summary>
             ///
-            /// <returns> Child 1. </returns>
-            int GetChild1() const;
-
-            /// <summary> Sets child 0. </summary>
-            ///
-            /// <param name="index"> Child 0. </param>
-            void SetChild0(int index);
-
-            /// <summary> Sets child 1. </summary>
-            ///
-            /// <param name="index"> Child 1. </param>
-            void SetChild1(int index);
+            /// <returns> Index of the positive child. </returns>
+            int GetPositiveChildIndex() const;
 
         private:
-            int _child0;
-            int _child1;
+            int _negativeChildIndex;
+            int _positiveChildIndex;
         };
-
-        /// <summary> Constructs an empty mutable decision tree with one leaf, the root. </summary>
-        ///
-        /// <param name="root_output"> The root output value. </param>
-        DecisionTree(double root_output = 0.0);
-
-        /// <summary> An iterator used to expose the tree split rules. </summary>
-        using SplitRuleIterator = std::vector<SplitRule>::const_iterator;
-
-        /// <summary>
-        /// Returns An iterator over the split rules that points to the first SplitRule.
-        /// </summary>
-        ///
-        /// <returns> A SplitRuleIterator. </returns>
-        SplitRuleIterator SplitRuleBegin() const;
-
-        /// <summary>
-        /// Returns An iterator over the split rules that points beyond the last SplitRule.
-        /// </summary>
-        ///
-        /// <returns> A SplitRuleIterator. </returns>
-        SplitRuleIterator SplitRuleEnd() const;
-
-        /// <summary>
-        /// An iterator used to expose the parent identities of vertices in the the tree.
-        /// </summary>
-        using ParentIterator = std::vector<int>::const_iterator;
-
-        /// <summary>
-        /// Returns An iterator over parent indentities that points to the first vertex.
-        /// </summary>
-        ///
-        /// <returns> A ParentIterator. </returns>
-        ParentIterator ParentBegin() const;
-
-        /// <summary>
-        /// Returns An iterator over parent identities that points beyond the last vertex.
-        /// </summary>
-        ///
-        /// <returns> A ParentIterator. </returns>
-        ParentIterator ParentEnd() const;
-
-        /// <summary> An iterator used to expose the ChildPair values of the tree. </summary>
-        using ChildrenIterator = std::vector<ChildPair>::const_iterator;
-
-        /// <summary> Returns An iterator over ChildPair values that points to the first vertex. </summary>
-        ///
-        /// <returns> A ChildrenIterator. </returns>
-        ChildrenIterator ChildrenBegin() const;
-
-        /// <summary>
-        /// Returns An iterator over ChildPair values that points beyond the last vertex.
-        /// </summary>
-        ///
-        /// <returns> A ChildrenIterator. </returns>
-        ChildrenIterator ChildrenEnd() const;
-
-        /// <summary> An iterator used to expose the output values of the tree. </summary>
-        using OutputIterator = std::vector<double>::const_iterator;
-
-        /// <summary> Returns An iterator over output values that points to the first vertex. </summary>
-        ///
-        /// <returns> An OutputIterator. </returns>
-        OutputIterator OutputBegin() const;
-
-        /// <summary> Returns An iterator over output values that points beyond the last vertex. </summary>
-        ///
-        /// <returns> An OutputIterator. </returns>
-        OutputIterator OutputEnd() const;
 
         /// <summary> Returns the number of vertices. </summary>
         ///
         /// <returns> The number vertices. </returns>
-        int GetNumVertices() const;
+        int NumVertices() const;
 
         /// <summary> Returns the number of interior vertices. </summary>
         ///
         /// <returns> The number interior vertices. </returns>
-        int GetNumInteriorVertices() const;
-
-        /// <summary> Performs a split of a leaf. </summary>
-        ///
-        /// <param name="leaf_index"> Zero-based index of the leaf. </param>
-        /// <param name="input_index"> Zero-based index of the input. </param>
-        /// <param name="treshold"> The treshold. </param>
-        /// <param name="child0_output"> The child 0 output. </param>
-        /// <param name="child1_output"> The child 1 output. </param>
-        void SplitLeaf(int leaf_index, 
-                      int input_index, 
-                      double treshold, 
-                      double child0_output, 
-                      double child1_output);
-
-        /// <summary> Deleted copy constructor. </summary>
-        DecisionTree(const DecisionTree&) = delete;
+        int NumInteriorVertices() const;
 
     private:
-        
         std::vector<SplitRule> _splitRules;
         std::vector<ChildPair> _children;
         std::vector<int> _parents;
--- libraries/predictors/src/DecisionTree.cpp
+++ libraries/predictors/src/DecisionTree.cpp
@@ -15,158 +15,41 @@
 
 namespace decisionTree
 {
-    DecisionTree::SplitRule::SplitRule(int index, double value) :
-        _index(index), _value(value)
+    DecisionTree::SplitRule::SplitRule(int featureIndex, double threshold) :
+        _featureIndex(featureIndex), _threshold(threshold)
     {}
 
-    int DecisionTree::SplitRule::GetIndex() const
+    int DecisionTree::SplitRule::GetFeatureIndex() const
     {
-        return _index;
+        return _featureIndex;
     }
 
-    double DecisionTree::SplitRule::Get() const
+    double DecisionTree::SplitRule::GetThreshold() const
     {
-        return _value;
+        return _threshold;
     }
 
-    DecisionTree::ChildPair::ChildPair(int child0, int child1) :
-        _child0(child0), _child1(child1)
+    DecisionTree::ChildPair::ChildPair(int negativeChildIndex, int positiveChildIndex) :
+        _negativeChildIndex(negativeChildIndex), _positiveChildIndex(positiveChildIndex)
     {}
 
-    int DecisionTree::ChildPair::GetChild0() const
+    int DecisionTree::ChildPair::GetNegativeChildIndex() const
     {
-        return _child0;
+        return _negativeChildIndex;
     }
 
-    int DecisionTree::ChildPair::GetChild1() const
+    int DecisionTree::ChildPair::GetPositiveChildIndex() const
     {
-        return _child1;
+        return _positiveChildIndex;
     }
 
-    void DecisionTree::ChildPair::SetChild0(int index)
-    {
-        _child0 = index;
-    }
-
-    void DecisionTree::ChildPair::SetChild1(int index)
-    {
-        _child1 = index;
-    }
-
-    DecisionTree::DecisionTree(double root_output) :
-        _parents(1), _outputs(1)
-    {
-        _parents[0] = -1;
-        _outputs[0] = root_output;
-    }
-
-    DecisionTree::SplitRuleIterator DecisionTree::SplitRuleBegin() const
-    {
-        return _splitRules.cbegin();
-    }
-
-    DecisionTree::SplitRuleIterator DecisionTree::SplitRuleEnd() const
-    {
-        return _splitRules.cend();
-    }
-
-    DecisionTree::ParentIterator DecisionTree::ParentBegin() const
-    {
-        return _parents.cbegin();
-    }
-
-    DecisionTree::ParentIterator DecisionTree::ParentEnd() const
-    {
-        return _parents.cend();
-    }
-
-    DecisionTree::ChildrenIterator DecisionTree::ChildrenBegin() const
-    {
-        return _children.cbegin();
-    }
-
-    DecisionTree::ChildrenIterator DecisionTree::ChildrenEnd() const
-    {
-        return _children.cend();
-    }
-
-    DecisionTree::OutputIterator DecisionTree::OutputBegin() const
-    {
-        return _outputs.cbegin();
-    }
-
-    DecisionTree::OutputIterator DecisionTree::OutputEnd() const
-    {
-        return _outputs.cend();
-    }
-
-    int DecisionTree::GetNumVertices() const
+    int DecisionTree::NumVertices() const
     {
         return (int)_parents.size();
     }
 
-    int DecisionTree::GetNumInteriorVertices() const
+    int DecisionTree::NumInteriorVertices() const
     {
         return (int)_splitRules.size();
-    }
-
-    void DecisionTree::SplitLeaf(
-        int leaf_index,
-        int input_index,
-        double threshold,
-        double child0_output,
-        double child1_output)
-    {
-
-        // check that the arguments are valid
-        if(leaf_index >= GetNumVertices() || leaf_index < GetNumInteriorVertices())
-        {
-            throw std::runtime_error("leaf index out of bounds");
-        }
-
-        // get new indices
-        int new_interior_index = GetNumInteriorVertices();
-        int child0_index = GetNumVertices();
-        int child1_index = child0_index + 1;
-
-        // std::swap leaf_index with new_interior_index
-        if (leaf_index != new_interior_index)
-        {
-
-            // tell the parents about the std::swap
-            int parent_of_leaf = _parents[leaf_index];
-            int parent_of_new_interior = _parents[new_interior_index];
-
-            if (_children[parent_of_leaf].GetChild0() == leaf_index)
-            {
-                _children[parent_of_leaf].SetChild0(new_interior_index);
-            }
-            else
-            {
-                _children[parent_of_leaf].SetChild1(new_interior_index);
-            }
-            if (_children[parent_of_new_interior].GetChild0() == new_interior_index)
-            {
-                _children[parent_of_new_interior].SetChild0(leaf_index);
-            }
-            else
-            {
-                _children[parent_of_new_interior].SetChild1(leaf_index);
-            }
-            
-            // std::swap the vertices
-            std::swap(_parents[leaf_index], _parents[new_interior_index]);
-            std::swap(_outputs[leaf_index], _outputs[new_interior_index]);
-        }
-
-        // Add new leaves
-        _outputs.push_back(child0_output);
-        _outputs.push_back(child1_output);
-        _parents.push_back(new_interior_index);
-        _parents.push_back(new_interior_index);
-
-        // Add new interior node
-        _children.emplace_back(child0_index, child1_index);
-        _splitRules.emplace_back(input_index, threshold);
     }
 }
