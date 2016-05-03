bc583b209f0a9d1f03f1f144d83c4e2134e44883
Charles Jacobs <cjacobs@mac.com>
2016-05-02 22:28:14 -0700

    Fixed style guide deviations

--- interfaces/common/include/MapInterface.h
+++ interfaces/common/include/MapInterface.h
@@ -45,7 +45,7 @@ public:
     /// <returns> An Iterator over output values. </returns>
     std::vector<double> Compute(const std::vector<double>& input) const;
 
-    uint64_t NumLayers() const;
+    //uint64_t NumLayers() const;
 
     /// <summary> Returns the current output coordinates for the map. </summary>
     ///
