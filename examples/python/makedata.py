2c3ee548ebbc65faca3bb0499193d86b1ce5c61f
Charles Jacobs <cjacobs@microsoft.com>
2016-05-12 15:17:48 -0700

    Added new synthetic dataset. Fixed tests that rely on datasets to use it.

--- examples/python/makedata.py
+++ examples/python/makedata.py
@@ -1,10 +1,21 @@
-import numpy
 
-def generateDataset():
-    numDim = 20
-    numSamples = 100 # (per label)
+def generateDataset(numSamplesPerLabel, numDim):
+    import numpy
     posCenter = numpy.random.uniform(-2, 2, numDim)
     negCenter = numpy.random.uniform(-2, 2, numDim)
-    posEx = numpy.random.normal(0.0, 1.0, (numSamples, numDim)) + posCenter
-    negEx = numpy.random.normal(0.0, 1.0, (numSamples, numDim)) + negCenter
-    # TODO: add pos and neg examples to a big list, randomize order, and output to a file    
+    posData = numpy.random.normal(0.0, 1.0, (numSamplesPerLabel, numDim)) + posCenter
+    posEx = numpy.hstack((numpy.ones((numSamplesPerLabel, 1)), posData))
+    negData = numpy.random.normal(0.0, 1.0, (numSamplesPerLabel, numDim)) + negCenter
+    negEx = numpy.hstack((-numpy.ones((numSamplesPerLabel, 1)), negData))
+    dataset = numpy.vstack((posEx, negEx))
+    numpy.random.shuffle(dataset)
+    return dataset
+
+def writeFile(dataset, filename):
+    with  open(filename, 'w') as fp:
+        for row in dataset:
+            fp.write('{}'.format(row[0]))
+            for i, v in enumerate(row[1:]):
+                fp.write('\t{}:{}'.format(i+1, v))
+            fp.write('\n')
+            
