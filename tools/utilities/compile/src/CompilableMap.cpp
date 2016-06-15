3f953041024e7aa20b7111c623aeea125bae713d
Umesh Madan <umeshma@microsoft.com>
2016-06-15 11:35:12 -0700

    Rolling back change to original compile - so we can have both running and compare.

--- tools/utilities/compile/src/CompilableMap.cpp
+++ tools/utilities/compile/src/CompilableMap.cpp
@@ -10,7 +10,6 @@
 #include "CompilableCoordinatewise.h"
 #include "CompilableSum.h"
 #include "DataFlowNode.h"
-#include "CodeEmitter.h"
 
 // utilities
 #include "IntegerStack.h"
@@ -29,169 +28,160 @@ const char* reallocationFormat = "    % = %; // coordinate (%,%), reassigning te
 
 const char* AllocateTempVariableAndGetFormat(DataFlowNode& targetNode, utilities::IntegerStack& stack)
 {
-    if (targetNode.IsInitialized() == true)
-    {
-        return incrementFormat;
-    }
+	if (targetNode.IsInitialized() == true)
+	{
+		return incrementFormat;
+	}
 
-    if (targetNode.HasFixedVariableName() == true)
-    {
-        return assignmentFormat;
-    }
+	if (targetNode.HasFixedVariableName() == true)
+	{
+		return assignmentFormat;
+	}
 
-    bool isNovel = stack.IsTopNovel();
-    targetNode.SetTempVariableIndex(stack.Pop());
+	bool isNovel = stack.IsTopNovel();
+	targetNode.SetTempVariableIndex(stack.Pop());
 
-    if (isNovel)
-    {
-        return allocationFormat;
-    }
-    else
-    {
-        return reallocationFormat;
-    }
+	if (isNovel)
+	{
+		return allocationFormat;
+	}
+	else
+	{
+		return reallocationFormat;
+	}
 }
 
-//void ProcessNode(DataFlowNode& currentNode, DataFlowGraph& graph, utilities::IntegerStack& stack, CodeGenerator& codeGen)
-void ProcessNode(DataFlowNode& currentNode, DataFlowGraph& graph, CodeEmitter& codeGen)
+void ProcessNode(DataFlowNode& currentNode, DataFlowGraph& graph, utilities::IntegerStack& stack, std::ostream& os)
 {
-    auto currentNodeVariableName = currentNode.GetVariableName();
+	auto currentNodeVariableName = currentNode.GetVariableName();
 
-    // for each action
-    while(currentNode.HasActions())
-    {
-        // get the next action in the current node
-        auto action = currentNode.PopAction();
+	// for each action
+	while (currentNode.HasActions())
+	{
+		// get the next action in the current node
+		auto action = currentNode.PopAction();
 
-        // get the target coordinate and target node
-        auto targetCoordinate = action.GetTarget();
-        auto& targetNode = graph.GetNode(targetCoordinate);
+		// get the target coordinate and target node
+		auto targetCoordinate = action.GetTarget();
+		auto& targetNode = graph.GetNode(targetCoordinate);
 
-        //const char* format = AllocateTempVariableAndGetFormat(targetNode, stack);
-        //auto targetVariableName = targetNode.GetVariableName();
-        //auto rhs = action.GetOperation().ToString(currentNodeVariableName);
-        //utilities::PrintFormat(os, format, targetVariableName, rhs, targetCoordinate.GetLayerIndex(), targetCoordinate.GetElementIndex());
+		const char* format = AllocateTempVariableAndGetFormat(targetNode, stack);
+		auto targetVariableName = targetNode.GetVariableName();
+		auto rhs = action.GetOperation().ToString(currentNodeVariableName);
 
-		codeGen.Emit(currentNodeVariableName, targetNode, action.GetOperation(), targetCoordinate.GetLayerIndex(), targetCoordinate.GetElementIndex());
+		utilities::PrintFormat(os, format, targetVariableName, rhs, targetCoordinate.GetLayerIndex(), targetCoordinate.GetElementIndex());
 
-        // indicate that the target node is initialized
-        targetNode.SetInitialized();
+		// indicate that the target node is initialized
+		targetNode.SetInitialized();
 
-        // check if temp variable can be released
-        if(currentNode.HasActions() == false && currentNode.HasTempVariableName())
-        {
-            //stack.Push(currentNode.GetTempVariableIndex());
-			codeGen.ReleaseVar(currentNode);
-        }
+		// check if temp variable can be released
+		if (currentNode.HasActions() == false && currentNode.HasTempVariableName())
+		{
+			stack.Push(currentNode.GetTempVariableIndex());
+		}
 
-        // if target node has all of its inputs, process it 
-        targetNode.DecrementUncomputedInputs();
-        if(targetNode.IsWaitingForInputs() == false)
-        {
-            //ProcessNode(targetNode, graph, stack, os);
-			ProcessNode(targetNode, graph, codeGen);
-        }
-    }
+		// if target node has all of its inputs, process it 
+		targetNode.DecrementUncomputedInputs();
+		if (targetNode.IsWaitingForInputs() == false)
+		{
+			ProcessNode(targetNode, graph, stack, os);
+		}
+	}
 }
 
 CompilableMap::CompilableMap(const layers::Map& other)
 {
-    _outputCoordinates = other.GetOutputCoordinateList();
+	_outputCoordinates = other.GetOutputCoordinateList();
 
-    const auto& model = other.GetModel();
-    _requiredInputLayerSize = std::max(model.GetRequiredLayerSize(0), _outputCoordinates.GetRequiredLayerSize(0));
+	const auto& model = other.GetModel();
+	_requiredInputLayerSize = std::max(model.GetRequiredLayerSize(0), _outputCoordinates.GetRequiredLayerSize(0));
 
-    utilities::TypeFactory<CompilableLayer> compilableLayerFactory;
-    compilableLayerFactory.AddType<CompilableCoordinatewise>(layers::Coordinatewise::GetTypeName());
-    compilableLayerFactory.AddType<CompilableSum>(layers::Sum::GetTypeName());
+	utilities::TypeFactory<CompilableLayer> compilableLayerFactory;
+	compilableLayerFactory.AddType<CompilableCoordinatewise>(layers::Coordinatewise::GetTypeName());
+	compilableLayerFactory.AddType<CompilableSum>(layers::Sum::GetTypeName());
 
-    // input layer is stored implicitly, copy all other layers
-    for (uint64_t index = 1; index < model.NumLayers(); ++index)
-    {
-        const auto& layer = model.GetLayer(index);
-        _compilableLayers.push_back(compilableLayerFactory.Construct(layer.GetRuntimeTypeName()));
-        (*_compilableLayers.back()) = layer;
-    }
+	// input layer is stored implicitly, copy all other layers
+	for (uint64_t index = 1; index < model.NumLayers(); ++index)
+	{
+		const auto& layer = model.GetLayer(index);
+		_compilableLayers.push_back(compilableLayerFactory.Construct(layer.GetRuntimeTypeName()));
+		(*_compilableLayers.back()) = layer;
+	}
 }
 
 void CompilableMap::ToCode(std::ostream& os) const
 {
-    uint64_t numLayersExcludingInput = _compilableLayers.size();
+	uint64_t numLayersExcludingInput = _compilableLayers.size();
 
-    // create data flow graph datastructure
-    DataFlowGraph graph;
+	// create data flow graph datastructure
+	DataFlowGraph graph;
 
-    // add graph layer for input
-    graph.AddLayer(_requiredInputLayerSize);
+	// add graph layer for input
+	graph.AddLayer(_requiredInputLayerSize);
 
-    // add graph later for other layers
-    for(uint64_t i = 0; i < numLayersExcludingInput; ++i)
-    {
-        graph.AddLayer(_compilableLayers[i]->GetOutputDimension());
-    }
+	// add graph later for other layers
+	for (uint64_t i = 0; i < numLayersExcludingInput; ++i)
+	{
+		graph.AddLayer(_compilableLayers[i]->GetOutputDimension());
+	}
 
-    // add an extra layer for outputs
-    uint64_t outputLayerSize = _outputCoordinates.Size();
-    uint64_t outputLayerIndex = numLayersExcludingInput+1;
-    graph.AddLayer(outputLayerSize);
+	// add an extra layer for outputs
+	uint64_t outputLayerSize = _outputCoordinates.Size();
+	uint64_t outputLayerIndex = numLayersExcludingInput + 1;
+	graph.AddLayer(outputLayerSize);
 
-    // set names on output layer and set the actions that generate the output coordinates
-    const std::string outputFixedVariableName = "output";
-    for (uint64_t outputElementIndex = 0; outputElementIndex < outputLayerSize; ++outputElementIndex)
-    {
-        auto inputCoordinate = _outputCoordinates[outputElementIndex];
-        layers::Coordinate outputCoordinate(outputLayerIndex, outputElementIndex);
-        graph.GetNode(outputCoordinate).SetFixedVariableName(outputFixedVariableName + "[" + std::to_string(outputElementIndex) + "]");
-        graph.GetNode(inputCoordinate).EmplaceAction(outputCoordinate);
-        graph.GetNode(outputCoordinate).IncrementUncomputedInputs();
-    }
+	// set names on output layer and set the actions that generate the output coordinates
+	const std::string outputFixedVariableName = "output";
+	for (uint64_t outputElementIndex = 0; outputElementIndex < outputLayerSize; ++outputElementIndex)
+	{
+		auto inputCoordinate = _outputCoordinates[outputElementIndex];
+		layers::Coordinate outputCoordinate(outputLayerIndex, outputElementIndex);
+		graph.GetNode(outputCoordinate).SetFixedVariableName(outputFixedVariableName + "[" + std::to_string(outputElementIndex) + "]");
+		graph.GetNode(inputCoordinate).EmplaceAction(outputCoordinate);
+		graph.GetNode(outputCoordinate).IncrementUncomputedInputs();
+	}
 
-    // backwards pass to assign actions to nodes
-    for(uint64_t layerIndex = outputLayerIndex-1; layerIndex > 0; --layerIndex)
-    {
-        _compilableLayers[layerIndex-1]->SetActions(layerIndex, graph);
-    }
+	// backwards pass to assign actions to nodes
+	for (uint64_t layerIndex = outputLayerIndex - 1; layerIndex > 0; --layerIndex)
+	{
+		_compilableLayers[layerIndex - 1]->SetActions(layerIndex, graph);
+	}
 
-    // construct an integer stack, to manage temp variable names;
-    //utilities::IntegerStack stack;
-    // print comment
-    //auto str = "// Predict function\n// Input dimension: %\n// Output dimension: %\n// Output coordinates:";
-    //utilities::PrintFormat(os, str, _requiredInputLayerSize, outputLayerSize);
-    //for (uint64_t i = 0; i < _outputCoordinates.Size(); ++i)
-    //{
-    //    os << ' ' << _outputCoordinates[i];
-    //}
+	// construct an integer stack, to manage temp variable names;
+	utilities::IntegerStack stack;
 
-    // print function declaration
-    //os << "\nvoid Predict(const double* input, double* output)\n{\n";
+	// print comment
+	auto str = "// Predict function\n// Input dimension: %\n// Output dimension: %\n// Output coordinates:";
+	utilities::PrintFormat(os, str, _requiredInputLayerSize, outputLayerSize);
+	for (uint64_t i = 0; i < _outputCoordinates.Size(); ++i)
+	{
+		os << ' ' << _outputCoordinates[i];
+	}
 
-    // forward pass to generate code
-	CEmitter codeGen(os);
-	codeGen.BeginLinear("Predict", _requiredInputLayerSize, _outputCoordinates);
+	// print function declaration
+	os << "\nvoid Predict(const double* input, double* output)\n{\n";
 
-    const std::string inputNamePrefix = "input";
+	// forward pass to generate code
+	const std::string inputNamePrefix = "input";
 	for (uint64_t inputElementIndex = 0; inputElementIndex < _requiredInputLayerSize; ++inputElementIndex)
-    {
-        layers::Coordinate inputCoordinate(0, inputElementIndex);
-        auto& inputNode = graph.GetNode(inputCoordinate);
+	{
+		layers::Coordinate inputCoordinate(0, inputElementIndex);
+		auto& inputNode = graph.GetNode(inputCoordinate);
 
-        std::string inputVariableName = inputNamePrefix + "[" + std::to_string(inputElementIndex) + "]";
+		std::string inputVariableName = inputNamePrefix + "[" + std::to_string(inputElementIndex) + "]";
 
-        // if input has multiple actions, first copy it to a temp variable. Otherwise, operate directly on the input array
-        if (inputNode.GetActions().size() <= 1)
-        {
-            inputNode.SetFixedVariableName(inputVariableName);
-        }
-        else
-        {
-            //const char* format = AllocateTempVariableAndGetFormat(inputNode, stack);
-            //utilities::PrintFormat(os, format, inputNode.GetVariableName(), inputVariableName, 0, inputElementIndex);
-			codeGen.Emit(inputVariableName, inputNode, 0, inputElementIndex);
-        }
-        //ProcessNode(inputNode, graph, stack, codeGen);
-		ProcessNode(inputNode, graph, codeGen);
-    }
+		// if input has multiple actions, first copy it to a temp variable. Otherwise, operate directly on the input array
+		if (inputNode.GetActions().size() <= 1)
+		{
+			inputNode.SetFixedVariableName(inputVariableName);
+		}
+		else
+		{
+			const char* format = AllocateTempVariableAndGetFormat(inputNode, stack);
+			utilities::PrintFormat(os, format, inputNode.GetVariableName(), inputVariableName, 0, inputElementIndex);
+		}
+		ProcessNode(inputNode, graph, stack, os);
+	}
 
-   // os << "}\n";
-	codeGen.EndLinear();
+	os << "}\n";
 }
