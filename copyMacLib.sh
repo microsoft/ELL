9cf03bd6c3b44f79f7c132a950b42e657cf43ece
Charles Jacobs <cjacobs@mac.com>
2016-07-10 22:30:25 -0700

    Now we record the git revision of the EMLL directory when building

--- copyMacLib.sh
+++ copyMacLib.sh
@@ -1,7 +1,5 @@
 mkdir -p ./dist/osx/build/Release
 
 cp -f build/Release/emll.node ./dist/osx/build/Release
-# cp -f package.json ./dist
-# cp -f index_multiplat.js ./dist/index.js
-# cp -f emll.d.ts ./dist
-# cp -f swig/EMLL_wrap.xml ./dist
+
+git -C ../EMLL rev-parse --short HEAD > ./dist/osx/EMLL_revision.txt
