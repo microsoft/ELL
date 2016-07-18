3e402385477bf23bed9d05c7c1c6fa44a09dbaa6
Charles Jacobs <cjacobs@microsoft.com>
2016-07-18 12:19:52 -0700

    Added OutputNode type

---
+++ libraries/model/include/OutputNode.h
@@ -0,0 +1,56 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Project:  Embedded Machine Learning Library (EMLL)
+//  File:     InputNode.h (model)
+//  Authors:  Chuck Jacobs
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include "Node.h"
+#include "OutputPort.h"
+#include "ModelTransformer.h"
+
+#include <vector>
+#include <memory>
+#include <string>
+
+/// <summary> model namespace </summary>
+namespace model
+{
+    /// <summary> A node that represents an input to the system. </summary>
+    template <typename ValueType>
+    class OutputNode : public Node
+    {
+    public:
+        /// <summary> Constructor </summary>
+        ///
+        /// <param name="dimension"> The input dimension </param>
+        OutputNode(const model::OutputPortElementList<ValueType>& input);
+
+        /// <summary> Gets the name of this type (for serialization). </summary>
+        ///
+        /// <returns> The name of this type. </returns>
+        static std::string GetTypeName() { return "OutputNode"; }
+
+        /// <summary> Gets the name of this type (for serialization). </summary>
+        ///
+        /// <returns> The name of this type. </returns>
+        virtual std::string GetRuntimeTypeName() const override { return GetTypeName(); }
+
+        /// <summary> Exposes the output port as a read-only property </summary>
+        const OutputPort<ValueType>& output = _output;
+
+        virtual void Copy(ModelTransformer& transformer) const override;
+
+    protected:
+        virtual void Compute() const override;
+
+    private:
+        InputPort<ValueType> _input;
+        OutputPort<ValueType> _output;
+    };
+}
+
+#include "../tcc/OutputNode.tcc"
---
+++ libraries/model/tcc/OutputNode.tcc
@@ -0,0 +1,28 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Project:  Embedded Machine Learning Library (EMLL)
+//  File:     OutputNode.tcc (model)
+//  Authors:  Chuck Jacobs
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/// <summary> model namespace </summary>
+namespace model
+{
+    template <typename ValueType>
+    OutputNode<ValueType>::OutputNode(const model::OutputPortElementList<ValueType>& input) : Node({&_input}, { &_output }), _input(this, input), _output(this, input.Size()){};
+
+    template <typename ValueType>
+    void OutputNode<ValueType>::Compute() const
+    {
+        _output.SetOutput(_input.GetValue());
+    }
+
+    template <typename ValueType>
+    void OutputNode<ValueType>::Copy(ModelTransformer& transformer) const
+    {
+        auto newInput = transformer.TransformInputPort(_input);
+        auto newNode = transformer.AddNode<OutputNode<ValueType>>(newInput);
+        transformer.MapOutputPort(output, newNode->output);
+    }
+}
