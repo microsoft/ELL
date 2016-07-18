d5e3ddf65a954a9513fa1621d8a26168c123e43d
Charles Jacobs <cjacobs@microsoft.com>
2016-07-18 16:23:26 -0700

    Added ISerializable files

---
+++ libraries/utilities/include/ISerializable.h
@@ -0,0 +1,34 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Project:  Embedded Machine Learning Library (EMLL)
+//  File:     ISerializable.h (utilities)
+//  Authors:  Chuck Jacobs
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include "Variant.h"
+
+#include <unordered_map>
+#include <string>
+#include <ostream>
+
+namespace utilities
+{
+    class ObjectDescription
+    {
+    public:
+        void AddField(std::string name, const Variant& value);
+        void Print(std::ostream& os, size_t indent = 0);
+
+    private:
+        std::unordered_map<std::string, Variant> _description;
+    };
+
+    class ISerializable
+    {
+    public:
+        virtual ObjectDescription GetDescription() const = 0;
+    };
+}
---
+++ libraries/utilities/src/ISerializable.cpp
@@ -0,0 +1,29 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Project:  Embedded Machine Learning Library (EMLL)
+//  File:     ISerializable.cpp (utilities)
+//  Authors:  Chuck Jacobs
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include "ISerializable.h"
+
+namespace utilities
+{
+    //
+    // ObjectDescription
+    //
+    void ObjectDescription::AddField(std::string name, const Variant& value)
+    {
+        _description.insert(std::make_pair(name, value)); 
+    }
+
+    void ObjectDescription::Print(std::ostream& os, size_t indent)
+    {
+        const std::string indentStr = std::string(indent, ' ');
+        for (const auto& pair : _description)
+        {
+//            os << indentStr << pair.first << ": " << to_string(pair.second) << std::endl;
+        }
+    }
+}
