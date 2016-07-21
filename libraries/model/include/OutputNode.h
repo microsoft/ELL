5c7b8c3c5606b1eee490e71ee62be9ac123b6bba
Charles Jacobs <cjacobs@mac.com>
2016-07-21 15:26:05 -0700

    Fixed OutputNode doc comments

--- libraries/model/include/OutputNode.h
+++ libraries/model/include/OutputNode.h
@@ -19,14 +19,14 @@
 /// <summary> model namespace </summary>
 namespace model
 {
-    /// <summary> A node that represents an input to the system. </summary>
+    /// <summary> A node that represents an output from the system. </summary>
     template <typename ValueType>
     class OutputNode : public Node
     {
     public:
         /// <summary> Constructor </summary>
         ///
-        /// <param name="dimension"> The input dimension </param>
+        /// <param name="input"> The node to get the input data from </param>
         OutputNode(const model::OutputPortElementList<ValueType>& input);
 
         /// <summary> Gets the name of this type (for serialization). </summary>
