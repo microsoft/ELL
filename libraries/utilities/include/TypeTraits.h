eb433fb5ee94913faa1b82f461343c859686ef2f
Charles Jacobs <cjacobs@mac.com>
2016-08-07 18:35:29 -0700

    Comments in TypeTraits.h

--- libraries/utilities/include/TypeTraits.h
+++ libraries/utilities/include/TypeTraits.h
@@ -14,6 +14,7 @@ namespace utilities
 {
     class ISerializable;
 
+    /// <summary> Utility class to test if a type is a specialization of std::vector. Used by IsVector/IsNotVector, below. </summary>
     template <typename ValueType>
     class IsVectorType
     {
@@ -33,24 +34,31 @@ namespace utilities
         static const bool value = IsVectorChecker<ValueType>(0);
     };
 
+    /// <summary> Enabled if ValueType is a fundamental value. </summary>
     template <typename ValueType>
     using IsFundamental = typename std::enable_if_t<std::is_fundamental<typename std::decay<ValueType>::type>::value, int>;
 
+    /// <summary> Enabled if ValueType is not a fundamental value. </summary>
     template <typename ValueType>
     using IsNotFundamental = typename std::enable_if_t<!std::is_fundamental<typename std::decay<ValueType>::type>::value, int>;
 
+    /// <summary> Enabled if ValueType is a class. </summary>
     template <typename ValueType>
     using IsClass = typename std::enable_if_t<std::is_class<ValueType>::value, int>;
 
+    /// <summary> Enabled if ValueType inherits from ISerializable. </summary>
     template <typename ValueType>
     using IsSerializable = typename std::enable_if_t<std::is_base_of<ISerializable, typename std::decay<ValueType>::type>::value, int>;
 
+    /// <summary> Enabled if ValueType does not inherit from ISerializable. </summary>
     template <typename ValueType>
     using IsNotSerializable = typename std::enable_if_t<(!std::is_base_of<ISerializable, typename std::decay<ValueType>::type>::value) && (!std::is_fundamental<typename std::decay<ValueType>::type>::value), int>;
 
+    /// <summary> Enabled if ValueType is a specialization of std::vector. </summary>
     template <typename ValueType>
     using IsVector = typename std::enable_if_t<IsVectorType<typename std::decay<ValueType>::type>::value, int>;
 
+    /// <summary> Enabled if ValueType is not a specialization of std::vector. </summary>
     template <typename ValueType>
     using IsNotVector = typename std::enable_if_t<!IsVectorType<typename std::decay<ValueType>::type>::value, int>;
 }
