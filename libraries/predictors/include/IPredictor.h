fc9b66a6ca6d229fd64d8d5e1d3252d18e166b30
Ofer Dekel <oferd@microsoft.com>
2016-08-14 00:22:48 -0700

    comment

--- libraries/predictors/include/IPredictor.h
+++ libraries/predictors/include/IPredictor.h
@@ -10,10 +10,15 @@
 
 namespace predictors
 {
+    /// <summary> Base class for predictors. </summary>
+    ///
+    /// <typeparam name="T"> The type of the predictions made by this predictor. </typeparam>
     template<typename T>
     class IPredictor
     {
     public:
+
+        /// <summary> The prediction type. </summary>
         using PredictionType = T;
     };
 }
