5a8b84f8a5dea18082b6b5db11a58569ab378bdc
Ofer Dekel <oferd@microsoft.com>
2016-08-30 12:35:04 -0700

    documentation

--- libraries/common/include/MakeTrainer.h
+++ libraries/common/include/MakeTrainer.h
@@ -33,7 +33,7 @@ namespace common
     /// <returns> A unique_ptr to a stochastic gradient descent trainer. </returns>
     std::unique_ptr<trainers::IIncrementalTrainer<predictors::LinearPredictor>> MakeSGDIncrementalTrainer(uint64_t dim, const LossArguments& lossArguments, const SGDIncrementalTrainerArguments& trainerArguments);
 
-    /// <summary> Makes a sorting forest trainerr. </summary>
+    /// <summary> Makes a sorting forest trainer. </summary>
     ///
     /// <param name="lossArguments"> loss arguments. </param>
     /// <param name="trainerArguments"> trainer arguments. </param>
@@ -41,7 +41,7 @@ namespace common
     /// <returns> A unique_ptr to a forest trainer. </returns>
     std::unique_ptr<trainers::IIncrementalTrainer<predictors::SimpleForestPredictor>> MakeSortingForestTrainer(const LossArguments& lossArguments, const ForestTrainerArguments& trainerArguments);
 
-    /// <summary> Makes a simple forest trainerr. </summary>
+    /// <summary> Makes a histogram forest trainer. </summary>
     ///
     /// <param name="lossArguments"> loss arguments. </param>
     /// <param name="trainerArguments"> trainer arguments. </param>
