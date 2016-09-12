9d1866a2e58f748f229086dabd3408e1c942f509
Kirk Olynyk <kirko@microsoft.com>
2016-09-12 13:26:12 -0700

    Stubbed out tests for all the libraries

---
+++ interfaces/python/test/evaluators_test.py
@@ -0,0 +1,6 @@
+import EMLL
+
+
+def test():
+    print "evaluators_test.test -- TBD"
+    return 0
--- interfaces/python/test/test.py
+++ interfaces/python/test/test.py
@@ -8,11 +8,25 @@ sys.path.append('./../Debug')
 import lossfunctions_test
 import model_test
 import common_test
+import trainers_test
+import predictors_test
+import nodes_test
+import linear_test
+import evaluators_test
+import dataset_test
+import treelayout_test
 
 tests = [   
             lossfunctions_test.test, 
             model_test.test, 
-            common_test.test    
+            common_test.test,
+            trainers_test.test,
+            predictors_test.test,
+            nodes_test.test,
+            linear_test.test,
+            evaluators_test.test,
+            dataset_test.test,
+            treelayout_test.test
         ]
 
 def interface_test():
