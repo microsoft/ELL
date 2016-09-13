79188d08b489c43ffb5b6b072c40f3ec3722f72a
Kirk Olynyk <kirko@microsoft.com>
2016-09-13 07:27:43 -0700

    Completed moving of personal batch files to dev/kirko

--- dev/kirko/setenv.cmd
+++ dev/kirko/setenv.cmd
@@ -5,4 +5,4 @@ doskey root=cd /d %cd%\..\..\$*
 doskey py=cd /d %cd%\..\..\interfaces\python\test\$*
 doskey libs=cd /d %cd%\..\..\libraries\$*
 doskey list=vim -R $*
-set PATH=%PATH%;c:\Program Files (x86)\MSBuild\14.0\Bin\
+set PATH=%PATH%;c:\Program Files (x86)\MSBuild\14.0\Bin\;%cd%
