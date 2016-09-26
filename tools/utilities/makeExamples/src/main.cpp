ce876aa5e5ec1a18c79741acd73b42e1432e636c
Charles Jacobs <cjacobs@microsoft.com>
2016-09-26 15:47:28 -0700

    Added utility to generate model files. Generates model files in examples directory as post-build step.

---
+++ tools/utilities/makeExamples/src/main.cpp
@@ -0,0 +1,103 @@
+
+//
+// Loading model tests
+//
+
+#include "GenerateModels.h"
+
+// common
+#include "LoadModel.h"
+
+// model
+#include "Model.h"
+
+// utilities
+#include "CommandLineParser.h"
+
+// stl
+#include <iostream>
+
+namespace emll
+{
+void SaveModels(std::string ext)
+{
+    auto model1 = GenerateModel1();
+    auto model2 = GenerateModel2();
+    auto model3 = GenerateModel3();
+    auto tree0 = GenerateTreeModel(0);
+    auto tree1 = GenerateTreeModel(1);
+    auto tree2 = GenerateTreeModel(2);
+    auto tree3 = GenerateTreeModel(3);
+
+    common::SaveModel(model1, "model_1." + ext);
+    common::SaveModel(model2, "model_2." + ext);
+    common::SaveModel(model3, "model_3." + ext);
+    common::SaveModel(tree0, "tree_0." + ext);
+    common::SaveModel(tree1, "tree_1." + ext);
+    common::SaveModel(tree2, "tree_2." + ext);
+    common::SaveModel(tree3, "tree_3." + ext);
+
+    // auto newModel1 = common::LoadModel("model_1." + ext);
+    // auto newModel2 = common::LoadModel("model_2." + ext);
+    // auto newModel3 = common::LoadModel("model_3." + ext);
+    // auto newTree0 = common::LoadModel("tree_0." + ext);
+    // auto newTree1 = common::LoadModel("tree_1." + ext);
+    // auto newTree2 = common::LoadModel("tree_2." + ext);
+    // auto newTree3 = common::LoadModel("tree_3." + ext);
+}
+}
+
+using namespace emll;
+
+int main(int argc, char* argv[])
+{
+    try
+    {
+        std::string format = "json";
+
+        // create a command line parser
+        utilities::CommandLineParser commandLineParser(argc, argv);
+        commandLineParser.AddOption(format, "modelFormat", "mf", "xml or json", "json");
+
+        // parse command line
+        commandLineParser.Parse();
+        if (format == "both")
+        {
+            SaveModels("xml");
+            SaveModels("json");
+        }
+        else
+        {
+            SaveModels(format);
+        }
+    }
+    catch (const utilities::CommandLineParserPrintHelpException& exception)
+    {
+        std::cout << exception.GetHelpText() << std::endl;
+        return 0;
+    }
+    catch (const utilities::CommandLineParserErrorException& exception)
+    {
+        std::cerr << "Command line parse error:" << std::endl;
+        for (const auto& error : exception.GetParseErrors())
+        {
+            std::cerr << error.GetMessage() << std::endl;
+        }
+        return 1;
+    }
+    catch (const utilities::Exception& exception)
+    {
+        std::cerr << "ERROR, got EMLL exception. Message: " << exception.GetMessage() << std::endl;
+        throw;
+    }
+    catch (const std::exception& exception)
+    {
+        std::cerr << "ERROR, got std exception. Message: " << exception.what() << std::endl;
+        throw;
+    }
+    catch (...)
+    {
+        std::cerr << "ERROR, got unknown exception." << std::endl;
+        throw;
+    }
+}
