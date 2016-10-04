28667d506f16b632a2a62d3beba8424e70467d39
Charles Jacobs <cjacobs@microsoft.com>
2016-10-04 12:31:45 -0700

    build/test working with CMake. Changed module directory.

--- interfaces/javascript/emll_module/buildme.cmd
+++ interfaces/javascript/emll_module/buildme.cmd
@@ -6,7 +6,7 @@ call npm install
 
 IF "%1"=="" (
   Echo building EMLL for Electron
-  call node-gyp rebuild
+  REM call node-gyp rebuild
   
   :: TODO: see if this step is really necessary. It isn't on OS X
   call node-gyp rebuild --target=0.36.7 --arch-x64 --dist-url=https://atom.io/download/atom-shell
@@ -17,4 +17,4 @@ IF /I "%1"=="node" (
   call node-gyp rebuild
 )
 
-:: call copyWindowsLib
+call copyWindowsLib
