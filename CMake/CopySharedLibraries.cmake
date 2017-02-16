38ed4f6fafc7d5a58fbbf4622b022abce533e36a
Charles Jacobs <cjacobs@mac.com>
2017-02-16 10:20:30 -0800

    Only copy DLLs if changed

--- CMake/CopySharedLibraries.cmake
+++ CMake/CopySharedLibraries.cmake
@@ -10,7 +10,7 @@ macro(copy_shared_libraries target_name)
                 add_custom_command(TARGET ${target_name} POST_BUILD
                     COMMAND ${CMAKE_COMMAND} -E make_directory ${target_location}
                     COMMAND ${CMAKE_COMMAND} -E echo "Copying DLL ${BLAS_DLL_DIR}/${blas_dll} to ${target_location}"
-                    COMMAND ${CMAKE_COMMAND} -E copy ${BLAS_DLL_DIR}/${blas_dll} ${target_location}
+                    COMMAND ${CMAKE_COMMAND} -E copy_if_different ${BLAS_DLL_DIR}/${blas_dll} ${target_location}
                 )
             endforeach()
         endif()
