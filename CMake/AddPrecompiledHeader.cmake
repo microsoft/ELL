abe7f7290baf5c13604064fec0192c7d02944ef5
Charles Jacobs <cjacobs@microsoft.com>
2017-02-23 14:11:42 -0800

    Debugging precomp headers

--- CMake/AddPrecompiledHeader.cmake
+++ CMake/AddPrecompiledHeader.cmake
@@ -7,6 +7,7 @@ function(add_precompiled_header target precompiled_header precompiled_source)
     get_filename_component(precompiled_basename ${precompiled_header} NAME_WE)
     set(precompiled_binary "$(IntDir)/${precompiled_basename}.pch")
     get_target_property(sources ${target} SOURCES)
+    message(STATUS "Sources before adding precomp header: ${sources}")
 
     set_source_files_properties(precompiled_source
                                 PROPERTIES COMPILE_FLAGS "/Yc\"${precompiled_header}\" /Fp\"${precompiled_binary}\""
@@ -20,5 +21,6 @@ function(add_precompiled_header target precompiled_header precompiled_source)
     target_sources(${target} PRIVATE ${precompiled_source})
 
     get_target_property(sources2 ${target} SOURCES)
+    message(STATUS "Sources after adding precomp header: ${sources2}")
   endif()
 endfunction()
