dba4c8b144362d61429f4a6e9a50ec500bed9621
Chuck Jacobs <cjacobs@microsoft.com>
2017-09-25 18:56:35 +0000

    Merged PR 498: Added a script to generate a compiled executable that behaves like the `profile` tool, but with a precompiled model
    Added a script that creates a compiled profiler project for a given model, target device, and compiler parameters.
    
    Added an "outputFilenameBase" option to the `compile` tool, so the above script would always know the names of the compiled model files.
    
    *Note:* This tool is pretty redundant with the "profile" option Lisa has added to demo.py, so I'm not sure how necessary it is. The python code is better in that it's easier to modify,has more flexible input options, and is better integrated with the "drive test" utility. OTOH, I do find it slightly more convenient to just run an executable rather than invoke the profiling via python, but maybe that's just because I'm used to this way of doing things.

---
+++ tools/utilities/profile/src/compiled_exercise_model_main.cpp
@@ -0,0 +1,82 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Project:  Embedded Learning Library (ELL)
+//  File:     compiled_exercise_model_main.cpp (profile)
+//  Authors:  Chuck Jacobs
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// compiled model
+#include "compiled_model_noprofile.h"
+
+// stl
+#include <cstring>
+#include <iostream>
+#include <random>
+#include <string>
+#include <vector>
+
+struct ProfileArguments
+{
+    int numIterations;
+    int numWarmUpIterations;
+};
+
+//
+// Test-data-related
+//
+template<typename T>
+std::vector<T> GetInputData(std::string filename, const TensorShape& inputShape, float scale)
+{
+    std::default_random_engine engine(123);
+    auto inputSize = inputShape.rows * inputShape.columns * inputShape.channels;
+    std::vector<T> result(inputSize);
+    std::uniform_real_distribution<double> dist;
+    for (auto index = 0; index < inputSize; ++index)
+    {
+        result[index] = static_cast<T>(dist(engine));
+    }
+    return result;
+}
+
+template<typename InputType, typename OutputType>
+void RunModel(const ProfileArguments& profileArguments)
+{
+    TensorShape inputShape;
+    TensorShape outputShape;
+    ELL_GetInputShape(0, &inputShape);
+    ELL_GetOutputShape(0, &outputShape);
+
+    auto inputSize = ELL_GetInputSize();
+    auto outputSize = ELL_GetOutputSize();
+
+    std::vector<InputType> input(inputSize);
+    std::vector<OutputType> output(outputSize);
+
+    // Evaluate the model in a loop
+    for (int iter = 0; iter < profileArguments.numIterations; ++iter)
+    {
+        // Exercise the model
+        ELL_predict(input.data(), output.data());
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    using InputType = float;
+    using OutputType = float;
+
+    int numIterations = 20;
+    if(argc > 1)
+    {
+        numIterations = atoi(argv[1]);
+    }
+
+    std::cout << "Performing " << numIterations << " iterations of predict function" << std::endl;
+    // add arguments to the command line parser
+    ProfileArguments profileArguments;
+    profileArguments.numIterations = numIterations;
+    RunModel<InputType, OutputType>(profileArguments);
+
+    return 0;
+}
---
+++ tools/utilities/profile/src/compiled_profile_main.cpp
@@ -0,0 +1,287 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Project:  Embedded Learning Library (ELL)
+//  File:     compiled_profile_main.cpp (profile)
+//  Authors:  Chuck Jacobs
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// compiled model
+#include "compiled_model.h"
+
+// stl
+#include <algorithm>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <random>
+#include <string>
+#include <vector>
+
+enum class ProfileOutputFormat
+{
+    text,
+    json
+};
+
+struct ProfileArguments
+{
+    std::string outputFilename;
+    std::string outputComment;
+    ProfileOutputFormat outputFormat;
+    int numIterations;
+    int numWarmUpIterations;
+};
+
+//
+// Test-data-related
+//
+template<typename T>
+std::vector<T> GetInputData(std::string filename, const TensorShape& inputShape, float scale)
+{
+    std::default_random_engine engine(123);
+    auto inputSize = inputShape.rows * inputShape.columns * inputShape.channels;
+    std::vector<T> result(inputSize);
+    std::uniform_real_distribution<double> dist;
+    for (auto index = 0; index < inputSize; ++index)
+    {
+        result[index] = static_cast<T>(dist(engine));
+    }
+    return result;
+}
+
+//
+// Output-related
+//
+void WriteUserComment(const std::string& comment, ProfileOutputFormat format, std::ostream& out)
+{
+    if (format == ProfileOutputFormat::text)
+    {
+        out << "Comment: " << comment << "\n";
+    }
+    else // json
+    {
+        out << "\"comment\": \"" << comment << "\"\n";
+    }
+}
+
+void WriteModelStatistics(ProfileOutputFormat format, std::ostream& out)
+{
+    // get overall stats
+    auto modelStats = ELL_GetModelPerformanceCounters();
+
+    if (format == ProfileOutputFormat::text)
+    {
+        std::ios::fmtflags savedFlags(out.flags());
+        out << std::fixed;
+        out.precision(5);
+
+        int count = modelStats->count;
+        double totalTime = modelStats->totalTime;
+        double timePerRun = totalTime / count;
+
+        out << "\nModel statistics" << std::endl;
+        out << "Total time: " << totalTime << " ms \tcount: " << count << "\t time per run: " << timePerRun << " ms" << std::endl;
+
+        out.flags(savedFlags);
+    }
+    else // json
+    {
+        int count = modelStats->count;
+        double totalTime = modelStats->totalTime;
+        double timePerRun = totalTime / count;
+
+        out << "\"model_statistics\": {\n";
+        out << "  \"total_time\": " << totalTime << ",\n";
+        out << "  \"average_time\": " << timePerRun << ",\n";
+        out << "  \"count\": " << count << "\n";
+        out << "}";
+    }
+}
+
+void WriteNodeStatistics(ProfileOutputFormat format, std::ostream& out)
+{
+    // Gather node statistics
+    std::vector<std::pair<ELL_NodeInfo, ELL_PerformanceCounters>> nodeInfo;
+    auto numNodes = ELL_GetNumNodes();
+    for (int index = 0; index < numNodes; ++index)
+    {
+        auto info = ELL_GetNodeInfo(index);
+        auto stats = ELL_GetNodePerformanceCounters(index);
+        nodeInfo.emplace_back(*info, *stats);
+    }
+
+    size_t maxTypeLength = 0;
+    std::vector<std::pair<ELL_NodeInfo, ELL_PerformanceCounters>> nodeTypeInfo;
+    auto numNodeTypes = ELL_GetNumNodeTypes();
+    for (int index = 0; index < numNodeTypes; ++index)
+    {
+        auto info = ELL_GetNodeTypeInfo(index);
+        auto stats = ELL_GetNodeTypePerformanceCounters(index);
+        nodeTypeInfo.emplace_back(*info, *stats);
+        maxTypeLength = std::max(maxTypeLength, std::strlen((const char*)(info->nodeType)));
+    }
+    std::sort(nodeTypeInfo.begin(), nodeTypeInfo.end(), [](auto a, auto b) { return a.second.totalTime < b.second.totalTime; });
+
+    // Write node statistics
+    if (format == ProfileOutputFormat::text)
+    {
+        std::ios::fmtflags savedFlags(out.flags());
+        out << std::fixed;
+        out.precision(5);
+
+        out << "Node statistics" << std::endl;
+        for (const auto& info : nodeInfo)
+        {
+            out << "Node[" << info.first.nodeName << "]:\t" << std::setw(maxTypeLength) << std::left << info.first.nodeType << "\ttime: " << info.second.totalTime << " ms\tcount: " << info.second.count << "\n";
+        }
+
+        out << "\n\n";
+        out << "Node type statistics" << std::endl;
+        for (const auto& info : nodeTypeInfo)
+        {
+            out << std::setw(maxTypeLength) << std::left << info.first.nodeType << "\ttime: " << info.second.totalTime << " ms \tcount: " << info.second.count << "\n";
+        }
+
+        out.flags(savedFlags);
+    }
+    else // json
+    {
+        out << "\"node_statistics\": [\n";
+        for (const auto& info : nodeInfo)
+        {
+            out << "  {\n";
+            out << "    \"name\": "
+                << "\"" << info.first.nodeName << "\",\n";
+            out << "    \"type\": "
+                << "\"" << info.first.nodeType << "\",\n";
+            out << "    \"total_time\": " << info.second.totalTime << ",\n";
+            out << "    \"average_time\": " << info.second.totalTime / info.second.count << ",\n";
+            out << "    \"count\": " << info.second.count << "\n";
+            out << "  }";
+            bool isLast = (&info == &nodeInfo.back());
+            if (!isLast)
+            {
+                out << ",";
+            }
+            out << "\n";
+        }
+        out << "],\n";
+
+        out << "\"node_type_statistics\": [\n";
+        out << "  [";
+        for (const auto& info : nodeTypeInfo)
+        {
+            out << "  {\n";
+            out << "    \"type\": "
+                << "\"" << info.first.nodeType << "\",\n";
+            out << "    \"total_time\": " << info.second.totalTime << ",\n";
+            out << "    \"average_time\": " << info.second.totalTime / info.second.count << ",\n";
+            out << "    \"count\": " << info.second.count << "\n";
+            out << "  }";
+            bool isLast = (&info == &nodeTypeInfo.back());
+            if (!isLast)
+            {
+                out << ",";
+            }
+            out << "\n";
+        }
+        out << "]";
+    }
+}
+
+//
+// Profiling functions
+//
+void ResetProfilingInfo()
+{
+    ELL_ResetModelProfilingInfo();
+    ELL_ResetNodeProfilingInfo();
+    ELL_ResetNodeTypeProfilingInfo();
+}
+
+template<typename InputType, typename OutputType>
+void ProfileModel(const ProfileArguments& profileArguments)
+{
+    auto& profileOutputStream = std::cout;
+    const auto comment = profileArguments.outputComment;
+
+    TensorShape inputShape;
+    TensorShape outputShape;
+    ELL_GetInputShape(0, &inputShape);
+    ELL_GetOutputShape(0, &outputShape);
+
+    auto inputSize = ELL_GetInputSize();
+    auto outputSize = ELL_GetOutputSize();
+
+    std::vector<InputType> input(inputSize);
+    std::vector<OutputType> output(outputSize);
+
+    // Warm up the system by evaluating the model some number of times
+    for (int iter = 0; iter < profileArguments.numWarmUpIterations; ++iter)
+    {
+        ELL_predict(input.data(), output.data());
+    }
+    ResetProfilingInfo();
+
+    // Now evaluate the model and record the profiling info
+    for (int iter = 0; iter < profileArguments.numIterations; ++iter)
+    {
+        // Exercise the model
+        ELL_predict(input.data(), output.data());
+    }
+
+    auto format = profileArguments.outputFormat;
+
+    // print profile info
+    if (format == ProfileOutputFormat::text)
+    {
+        if (!comment.empty())
+        {
+            WriteUserComment(comment, format, profileOutputStream);
+        }
+        WriteNodeStatistics(format, profileOutputStream);
+        WriteModelStatistics(format, profileOutputStream);
+    }
+    else
+    {
+        profileOutputStream << "{\n";
+        if (!comment.empty())
+        {
+            WriteUserComment(comment, format, profileOutputStream);
+            profileOutputStream << ",\n";
+        }
+        WriteNodeStatistics(format, profileOutputStream);
+        profileOutputStream << ",\n";
+        WriteModelStatistics(format, profileOutputStream);
+        profileOutputStream << "}\n";
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    using InputType = float;
+    using OutputType = float;
+
+    int numIterations = 20;
+    int numWarmUpIterations = 10;
+    if(argc > 1)
+    {
+        numIterations = atoi(argv[1]);
+    }
+    if(argc > 2)
+    {
+        numWarmUpIterations = atoi(argv[2]);
+    }
+
+    std::cout << "Profiling model with " << numWarmUpIterations << " warm-up iterations, and " << numIterations << " timed iterations" << std::endl;
+
+    // add arguments to the command line parser
+    ProfileArguments profileArguments;
+    profileArguments.numWarmUpIterations = numWarmUpIterations;
+    profileArguments.numIterations = numIterations;
+    profileArguments.outputFormat = ProfileOutputFormat::text;
+    ProfileModel<InputType, OutputType>(profileArguments);
+
+    return 0;
+}
