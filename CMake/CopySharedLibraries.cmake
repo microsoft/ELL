6976e1f3c6a23a99fdf521bf67f4796bdac115f6
Chris Lovett <clovett@microsoft.com>
2018-09-26 18:34:27 +0000

    Merged PR 1025: cleanup copy_newer_files pattern in our cmake files.
    I found the problem that keeps causing cmake to rerun it's generation phase. The problem seems to be with 'configure_file' commands (at least on Windows) where changing any one of those files causes cmake to re-run it's very slow "generation" phase.  It is better to use a new macro "copy_newer_files" which uses a custom target with a copy command to copy those files.  This way cmake regeneration does not happen because the copy happens at build time instead of cmake generation time.  For example, the cntk importer CMakeLists.txt contains this:
    ```cmake
        foreach(PY_FILE ${importer_src})
            configure_file(${PY_FILE} ${PY_FILE} COPYONLY)
        endforeach()
    ```
    and I would change it to this:
    ```cmake
        copy_newer_files(${module_name} ${importer_src})
    ```
    
    Related work items: #1657

--- CMake/CopySharedLibraries.cmake
+++ CMake/CopySharedLibraries.cmake
@@ -24,3 +24,26 @@ macro(set_test_library_path test_name)
         set_tests_properties(${test_name} PROPERTIES ENVIRONMENT "PATH=${GLOBAL_BIN_DIR}/$<CONFIG>/;%PATH%")
     endif()
 endmacro()
+
+macro(copy_newer_files target_name file_list)
+    set(out_dir ${CMAKE_CURRENT_BINARY_DIR})
+
+    # see if optional 3rd argument specified which provides alternate output dir
+    set(extra_macro_args ${ARGN})
+    list(LENGTH extra_macro_args num_extra_args)
+    if (${num_extra_args} GREATER 0)
+        list(GET extra_macro_args 0 out_dir)
+        add_custom_target(${target_name}_make_directory ALL 
+            COMMAND ${CMAKE_COMMAND} -E make_directory ${out_dir})
+    endif()
+
+    foreach(file_name ${${file_list}})
+        # note: this works better than configure_file.  configure_file causes cmake regeneration
+        # any time you touch any file used by configure_file, which makes incremental build of 
+        # such projects slow and painful.
+        add_custom_command(TARGET ${target_name} 
+            POST_BUILD
+            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+            COMMAND ${CMAKE_COMMAND} -E copy_if_different ${file_name} ${out_dir})
+    endforeach()
+endmacro()
