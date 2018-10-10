d56c9d8e36d24242a87f7b2b0a4e7d106ec0300c
Chuck Jacobs <cjacobs@microsoft.com>
2018-10-10 19:07:18 +0000

    Merged PR 1040: Fix some version-testing bugs in Ubuntu setup script
    The code that checks for prerequisites was reinstalling things (CMake, SWIG, and miniconda) when a newer version than the minimum required version was installed.
    
    It also turns off "verbose" mode when unarchiving `tar` archives.

--- SETUP-Ubuntu.sh
+++ SETUP-Ubuntu.sh
@@ -11,6 +11,8 @@ PYTHON_VERSION=$MIN_PYTHON_VERSION
 CMAKE_VERSION=3.12.0
 SWIG_VERSION=$MIN_SWIG_VERSION
 
+TMPDIR=mktemp -d
+
 sudo apt-get install software-properties-common
 
 if ! grep -q "^deb .*ubuntu-toolchain-r/test" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
@@ -25,51 +27,51 @@ sudo apt-get install -y gcc-8 g++-8 cmake libedit-dev zlibc zlib1g zlib1g-dev li
 
 # Check for cmake version
 cmake_ver=$(cmake --version 2>&1 | head -n1 | cut -d ' ' -f3 | awk '{print $NF}')
-if [ ! $cmake_ver = $MIN_CMAKE_VERSION ] && printf '%s\n%s\n' $cmake_ver $MIN_CMAKE_VERSION | sort -Cg; then
-    curl -o ${HOME}/cmake-$CMAKE_VERSION.tar.gz --location https://cmake.org/files/v$(echo $CMAKE_VERSION | cut -d '.' -f1-2 | awk '{print $NF}')/cmake-$CMAKE_VERSION.tar.gz
-    tar zxvf ${HOME}/cmake-$CMAKE_VERSION.tar.gz -C ${HOME}
-    pushd ${HOME}/cmake-$CMAKE_VERSION
-    ./configure && make && sudo make install
+if [ ! $cmake_ver = $MIN_CMAKE_VERSION ] && [ $(printf '%s\n%s\n' $cmake_ver $MIN_CMAKE_VERSION | sort -Cg) ] ; then
+    echo "Downloading and installing CMake"
+    cmake_short_version=$(echo ${CMAKE_VERSION} | cut -d '.' -f1-2 | awk '{print $NF}')
+    cmake_installer_file="cmake-${CMAKE_VERSION}-Linux-x86_64.sh"
+    curl -o "TMPDIR/${cmake_installer_file}" --location "https://cmake.org/files/v${cmake_short_version}/${cmake_installer_file}"
+    pushd "${TMPDIR}"
+    sudo sh "./${cmake_installer_file}" --skip-license --prefix=/usr --exclude-subdir
     popd
-    rm ${HOME}/cmake-$CMAKE_VERSION.tar.gz
 fi
 
 # Check for swig version
 swig_ver=$(swig -version 2>&1 | head -n2 | cut -d ' ' -f3 | awk '{print $NF}')
-if [ ! $swig_ver = $MIN_SWIG_VERSION ] && printf '%s\n%s\n' $swig_ver $MIN_SWIG_VERSION | sort -Cg; then
-    curl -o ${HOME}/swig-$SWIG_VERSION.tar.gz --location http://prdownloads.sourceforge.net/swig/swig-$SWIG_VERSION.tar.gz
-    tar zxvf ${HOME}/swig-$SWIG_VERSION.tar.gz -C ${HOME}
-    pushd ${HOME}/swig-$SWIG_VERSION
+if [ ! $swig_ver = $MIN_SWIG_VERSION ] && [ $(printf '%s\n%s\n' $swig_ver $MIN_SWIG_VERSION | sort -Cg) ] ; then
+    echo "Downloading and building SWIG"
+    curl -o "${TMPDIR}/swig-${SWIG_VERSION}.tar.gz" --location "http://prdownloads.sourceforge.net/swig/swig-${SWIG_VERSION}.tar.gz"
+    tar zxvf "${TMPDIR}/swig-${SWIG_VERSION}.tar.gz" -C ${tmpdit}
+    pushd "${TMPDIR}/swig-${SWIG_VERSION}"
     ./configure --without-pcre && make && sudo make install
     popd
-    rm ${HOME}/swig-$SWIG_VERSION.tar.gz
 fi
 
 # Check for miniconda and python version
 python_ver=$(python --version 2>&1 | head -n1 | cut -d ' ' -f2 | awk '{print $NF}')
-if [ ! $python_ver = $MIN_PYTHON_VERSION ] && printf '%s\n%s\n' $python_ver $MIN_PYTHON_VERSION | sort -Cg; then
+if [ ! $python_ver = $MIN_PYTHON_VERSION ] && [ $(printf '%s\n%s\n' $python_ver $MIN_PYTHON_VERSION | sort -Cg) ] ; then
+    echo "Downloading and installing miniconda"
     sys=$(arch)
     if [ "${sys}" = "x86" ]; then
-        curl -o ${HOME}/Miniconda.sh --location https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86.sh
+        curl -o "${TMPDIR}/Miniconda.sh" --location https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86.sh
     elif [ "${sys}" = "x86_64" ]; then
-        curl -o ${HOME}/miniconda.sh --location https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+        curl -o "${TMPDIR}/miniconda.sh" --location https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
     else
-        echo Cannot install miniconda
+        echo "Cannot install miniconda"
         exit 1
     fi
 
-    bash ${HOME}/miniconda.sh -b
-    rm ${HOME}/miniconda.sh
+    bash "${TMPDIR}/miniconda.sh" -b
+    rm "${TMPDIR}/miniconda.sh"
 
-    echo adding miniconda path to ~/.bashrc
+    echo "Adding miniconda path to ~/.bashrc"
     echo 'export PATH="$HOME/miniconda3/bin:$PATH"' >> ~/.bashrc
 
     ${HOME}/miniconda3/bin/conda update --update-deps -y -n base conda
     ${HOME}/miniconda3/bin/conda create -n py36 numpy python=$PYTHON_VERSION -y
     source ${HOME}/miniconda3/bin/activate py36
     ${HOME}/miniconda3/bin/conda install -c conda-forge opencv -y
-else
-    source activate py36
 fi
 
-set +echo
+rm -rf "${TMPDIR}"
