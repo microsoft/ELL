bd73d14d341950254153f8d42877ab9bd133f86f
Chuck Jacobs <cjacobs@microsoft.com>
2019-04-25 21:41:58 +0000

    Merged PR 1235: Fix Mac build error
    This PR fixes a build error on the Mac that crept in (having to do with how `uint64_t` is defined). Plus, it reduces some repeated boilerplate-y code.
    
    Related work items: #2151

--- libraries/utilities/src/CStringParser.cpp
+++ libraries/utilities/src/CStringParser.cpp
@@ -39,8 +39,8 @@ namespace utilities
         return std::isdigit(c) != 0;
     }
 
-    // wrapper for strtof
-    inline ParseResult cParse(const char* pStr, char*& pEnd, float& value)
+    template <typename ValueType, typename ParseFunctionType>
+    ParseResult ParseFloat(const char* pStr, char*& pEnd, ParseFunctionType parse, ValueType& value)
     {
         if (IsWhitespace(*pStr))
         {
@@ -50,7 +50,7 @@ namespace utilities
         auto tmp = errno;
         errno = 0;
 
-        value = strtof(pStr, &pEnd);
+        value = parse(pStr, &pEnd);
 
         if (pStr == pEnd)
         {
@@ -62,37 +62,11 @@ namespace utilities
         }
 
         errno = tmp;
-        return ParseResult::success;
+        return ParseResult::success;        
     }
 
-    // wrapper for std::strtod
-    inline ParseResult cParse(const char* pStr, char*& pEnd, double& value)
-    {
-        if (IsWhitespace(*pStr))
-        {
-            return ParseResult::badFormat;
-        }
-
-        auto tmp = errno;
-        errno = 0;
-
-        value = std::strtod(pStr, &pEnd);
-
-        if (pStr == pEnd)
-        {
-            return ParseResult::badFormat;
-        }
-        if (errno == ERANGE)
-        {
-            return ParseResult::outOfRange;
-        }
-
-        errno = tmp;
-        return ParseResult::success;
-    }
-
-    // wrapper for strtoul
-    inline ParseResult cParse(const char* pStr, char*& pEnd, unsigned int& value)
+    template <typename ValueType, typename ParseFunctionType>
+    ParseResult ParseInt(const char* pStr, char*& pEnd, ParseFunctionType parse, ValueType& value)
     {
         if (!IsDigit(*pStr))
         {
@@ -102,103 +76,13 @@ namespace utilities
         auto tmp = errno;
         errno = 0;
 
-        auto x = strtoul(pStr, &pEnd, 0);
-        if (x != static_cast<unsigned int>(x))
+        auto x = parse(pStr, &pEnd, 0);
+        if (x != static_cast<ValueType>(x))
         {
             return ParseResult::outOfRange;
         }
 
-        value = static_cast<unsigned int>(x);
-
-        if (pStr == pEnd)
-        {
-            return ParseResult::badFormat;
-        }
-        if (errno == ERANGE)
-        {
-            return ParseResult::outOfRange;
-        }
-
-        errno = tmp;
-        return ParseResult::success;
-    }
-
-    // wrapper for strtoul
-    inline ParseResult cParse(const char* pStr, char*& pEnd, uint64_t& value)
-    {
-        if (!IsDigit(*pStr))
-        {
-            return ParseResult::badFormat;
-        }
-
-        auto tmp = errno;
-        errno = 0;
-
-        auto x = strtoul(pStr, &pEnd, 0);
-
-        if (pStr == pEnd)
-        {
-            return ParseResult::badFormat;
-        }
-        if (errno == ERANGE)
-        {
-            return ParseResult::outOfRange;
-        }
-        if (x != static_cast<uint64_t>(x))
-        {
-            return ParseResult::outOfRange;
-        }
-
-        value = static_cast<uint64_t>(x);
-
-        errno = tmp;
-        return ParseResult::success;
-    }
-
-    // wrapper for strtol
-    inline ParseResult cParse(const char* pStr, char*& pEnd, int& value)
-    {
-        if (IsWhitespace(*pStr))
-        {
-            return ParseResult::badFormat;
-        }
-
-        auto tmp = errno;
-        errno = 0;
-
-        auto x = strtol(pStr, &pEnd, 0);
-
-        if (pStr == pEnd)
-        {
-            return ParseResult::badFormat;
-        }
-        if (errno == ERANGE)
-        {
-            return ParseResult::outOfRange;
-        }
-        if (x != static_cast<int>(x))
-        {
-            return ParseResult::outOfRange;
-        }
-
-        value = static_cast<int>(x);
-
-        errno = tmp;
-        return ParseResult::success;
-    }
-
-    // wrapper for strtol
-    inline ParseResult cParse(const char* pStr, char*& pEnd, long& value)
-    {
-        if (IsWhitespace(*pStr))
-        {
-            return ParseResult::badFormat;
-        }
-
-        auto tmp = errno;
-        errno = 0;
-
-        value = strtol(pStr, &pEnd, 0);
+        value = static_cast<ValueType>(x);
 
         if (pStr == pEnd)
         {
@@ -213,67 +97,52 @@ namespace utilities
         return ParseResult::success;
     }
     
+    // wrapper for std::strtof
+    inline ParseResult cParse(const char* pStr, char*& pEnd, float& value)
+    {
+        return ParseFloat(pStr, pEnd, std::strtof, value);
+    }
+
+    // wrapper for std::strtod
+    inline ParseResult cParse(const char* pStr, char*& pEnd, double& value)
+    {
+        return ParseFloat(pStr, pEnd, std::strtod, value);
+    }
+
+    // wrapper for strtoul
+    inline ParseResult cParse(const char* pStr, char*& pEnd, unsigned int& value)
+    {
+        return ParseInt(pStr, pEnd, std::strtoul, value);
+    }
+
+    // wrapper for strtoull
+    inline ParseResult cParse(const char* pStr, char*& pEnd, uint64_t& value)
+    {
+        return ParseInt(pStr, pEnd, std::strtoull, value);
+    }
+
+    // wrapper for strtol
+    inline ParseResult cParse(const char* pStr, char*& pEnd, int& value)
+    {
+        return ParseInt(pStr, pEnd, std::strtol, value);
+    }
+
+    // wrapper for strtol
+    inline ParseResult cParse(const char* pStr, char*& pEnd, long& value)
+    {
+        return ParseInt(pStr, pEnd, std::strtol, value);
+    }
+    
     // wrapper for strtoul
     inline ParseResult cParse(const char* pStr, char*& pEnd, unsigned short& value)
     {
-        if (!IsDigit(*pStr))
-        {
-            return ParseResult::badFormat;
-        }
-
-        auto tmp = errno;
-        errno = 0;
-
-        auto x = strtoul(pStr, &pEnd, 0);
-
-        if (pStr == pEnd)
-        {
-            return ParseResult::badFormat;
-        }
-        if (errno == ERANGE)
-        {
-            return ParseResult::outOfRange;
-        }
-        if (x != static_cast<unsigned short>(x))
-        {
-            return ParseResult::outOfRange;
-        }
-        value = static_cast<unsigned short>(x);
-
-        errno = tmp;
-        return ParseResult::success;
+        return ParseInt(pStr, pEnd, std::strtoul, value);
     }
 
     // wrapper for strtol
     inline ParseResult cParse(const char* pStr, char*& pEnd, short& value)
     {
-        if (IsWhitespace(*pStr))
-        {
-            return ParseResult::badFormat;
-        }
-
-        auto tmp = errno;
-        errno = 0;
-
-        long x = strtol(pStr, &pEnd, 0);
-
-        if (pStr == pEnd)
-        {
-            return ParseResult::badFormat;
-        }
-        if (errno == ERANGE)
-        {
-            return ParseResult::outOfRange;
-        }
-        if (x != static_cast<short>(x))
-        {
-            return ParseResult::outOfRange;
-        }
-
-        value = static_cast<short>(x);
-
-        errno = tmp;
-        return ParseResult::success;
+        return ParseInt(pStr, pEnd, std::strtol, value);
     }
 
     // parse a single char from the input string.
@@ -302,27 +171,14 @@ namespace utilities
     template <typename std::enable_if_t<!std::is_same<unsigned long, unsigned int>::value, int> = 0>
     inline ParseResult cParse(const char* pStr, char*& pEnd, unsigned long& value)
     {
-        if (!IsDigit(*pStr))
-        {
-            return ParseResult::badFormat;
-        }
+        return ParseInt(pStr, pEnd, std::strtoul, value);
+    }
 
-        auto tmp = errno;
-        errno = 0;
-
-        value = strtoul(pStr, &pEnd, 0);
-
-        if (pStr == pEnd)
-        {
-            return ParseResult::badFormat;
-        }
-        if (errno == ERANGE)
-        {
-            return ParseResult::outOfRange;
-        }
-
-        errno = tmp;
-        return ParseResult::success;
+    // wrapper for strtol
+    template <typename std::enable_if_t<!std::is_same<long, int>::value, int> = 0>
+    inline ParseResult cParse(const char* pStr, char*& pEnd, long& value)
+    {
+        return ParseInt(pStr, pEnd, std::strtoul, value);
     }
 
     template <typename ValueType>
@@ -361,13 +217,18 @@ namespace utilities
     template ParseResult Parse(const char*& pStr, float& value);
     template ParseResult Parse(const char*& pStr, double& value);
     template ParseResult Parse(const char*& pStr, unsigned int& value);
-    template ParseResult Parse(const char*& pStr, uint64_t& value);
     template ParseResult Parse(const char*& pStr, int& value);
+    template ParseResult Parse(const char*& pStr, uint64_t& value);
     template ParseResult Parse(const char*& pStr, long& value);
     template ParseResult Parse(const char*& pStr, unsigned short& value);
     template ParseResult Parse(const char*& pStr, short& value);
     template ParseResult Parse(const char*& pStr, char& value);
     template ParseResult Parse(const char*& pStr, std::string& value);
 
+// On most platforms, uint64_t is an alias to `unsigned long`. However, on macOS, uint64_t is an alias of `unsigned long long`,
+// so we need to add 'unsigned long' when compiling on the Mac to make sure it is archivable.
+#if defined(__APPLE__)
+    template ParseResult Parse(const char*& pStr, unsigned long& value);
+#endif
 } // namespace utilities
 } // namespace ell
