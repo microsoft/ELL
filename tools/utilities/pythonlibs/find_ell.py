9573e36f6ca227e9a6ed1ee2c30ac0b3af312380
Kern Handa (KERN) <kerha@microsoft.com>
2019-05-17 21:21:15 +0000

    Merged PR 1263: Modify find_ell.py to look in CWD first to find build directory
    This is needed because when using some IDEs that create their own build
    directory, such as VS 2017 or 2019, it doesn't match the expected pattern
    of <ELL_ROOT>/build. This way, if you're already in the build directory and
    try to run ELL python scripts, we'll behave correctly.

--- tools/utilities/pythonlibs/find_ell.py
+++ tools/utilities/pythonlibs/find_ell.py
@@ -28,27 +28,35 @@ def get_ell_root():
 def find_ell_build():
     """ find the ELL build directory relative to this file """
     build_dir = ""
-    head, tail = os.path.split(__this_file_directory)
-    while (tail != ""):
-        # find a file that is unique to the ELL build folder
-        # (this works if our search started somewhere in the build folder)
-        test = os.path.join(head, "ell_build_tools.json")
-        if (os.path.isfile(test)):
-            return head
+    for potential_dir in [os.getcwd(), __this_file_directory]:
+        head = potential_dir
+        tail = ""
+        while True:
+            # find a file that is unique to the ELL build folder
+            # (this works if our search started somewhere in the build folder)
+            test = os.path.join(head, "ell_build_tools.json")
+            if os.path.isfile(test):
+                return head
 
-        # find a file that is unique to the ELL repo root.
-        test = os.path.join(head, "StyleGuide.md")
-        if (os.path.isfile(test)):
-            # this happens if we are searching from outside the build folder, so in
-            # order to support different named build folders, like 'build_gcc' or 'build_clang'
-            # we match by "build" prefix.
-            for d in os.listdir(head):
-                dd = os.path.join(head, d)
-                if (d.startswith("build") and os.path.isdir(dd) and os.path.isfile(os.path.join(dd, "ell_build_tools.json"))):  # noqa: E501
-                    return dd
-            raise Exception("ELL build folder not found in {}\nFound:{}".format(head, os.listdir(head)))
+            # find a file that is unique to the ELL repo root.
+            test = os.path.join(head, "StyleGuide.md")
+            if os.path.isfile(test):
+                # this happens if we are searching from outside the build folder, so in
+                # order to support different named build folders, like 'build_gcc' or 'build_clang'
+                # we match by "build" prefix.
+                for d in os.listdir(head):
+                    dd = os.path.join(head, d)
+                    if d.startswith("build") and \
+                        os.path.isdir(dd) and \
+                            os.path.isfile(os.path.join(dd, "ell_build_tools.json")):  # noqa: E501
+                        return dd
+                print("ELL build folder not found in {}\nFound:{}".format(head, os.listdir(head)))
 
-        head, tail = os.path.split(head)
+            newhead, tail = os.path.split(head)
+            if newhead == head:
+                break
+            else:
+                head = newhead
     return build_dir
 
 
@@ -61,7 +69,10 @@ def add_ell_root_args(arg_parser):
 
 ell_build_dir = find_ell_build()
 if not ell_build_dir:
-  ell_build_dir = os.path.join(os.getenv("ELL_ROOT"), "build")
+    ell_root = os.getenv("ELL_ROOT")
+    if not ell_root:
+        raise ImportError("Could not determine ELL build directory and ELL_ROOT environment variable isn't set")
+    ell_build_dir = os.path.join(ell_root, "build")
 ell_py_dir = os.path.join(ell_build_dir, "interfaces", "python", "package")
 if ell_py_dir is None:
     raise ImportError("Could not find ell package, did you follow the ELL Python Binding build instructions?")
